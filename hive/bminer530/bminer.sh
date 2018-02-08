6b254b41241555ffe2079bcfe5eb936d12167f9c
dimafern <dimafern@localhost>
2018-02-08 19:50:45 +0000

    0.5-27 claymore 11.0

---
+++ hive/bminer530/bminer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+MINER_CONFIG_FILE=`dirname $0`/bminer.conf
+
+[[ ! -e $MINER_CONFIG_FILE ]] && echo "No $MINER_CONFIG_FILE, exiting" && exit 1
+
+conf=`cat $MINER_CONFIG_FILE | grep -v "^$" | grep -v "^#"`
+
+echo $conf
+./bminer `echo $conf` 2>&1 | tee bminer.log
+
