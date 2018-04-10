4d257016091242f62313b4ffbc2ed42d430ae6b1
Dmitriy Aleshkowsky <to.wwarlock@gmail.com>
2018-04-10 18:04:16 +0300

    The fix of "monero" option for users which doesn't use quotes.

--- hive/sgminer/sgminer.sh
+++ hive/sgminer/sgminer.sh
@@ -23,7 +23,7 @@ echo "Fork version: $binary"
 # TODO remove this then sgminer-gm will be fixed
 # A little hack to fix monero:true parameter
 SGMINER_GM_CLI_ARGS=""
-SGMINER_GM_ARGS_MONERO=$(cat sgminer.conf | grep -Pow '\"monero\"\s*:\s*\"true\"' | wc -l)
+SGMINER_GM_ARGS_MONERO=$(cat sgminer.conf | grep -Pow '\"monero\"\s*:.*true' | wc -l)
 if [ $SGMINER_GM_ARGS_MONERO -ne 0 ]; then
   SGMINER_GM_CLI_ARGS=" --monero"
 fi
