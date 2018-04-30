ac0198b4885f81778dc35adf3c976d7782c0f717
Fern <fern@kgroup.biz>
2018-04-30 23:14:29 +0300

    0.5-48 OhGodAnETHlargementPill, clyamore 11.7

---
+++ hive/opt/ohgodapill/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#. colors
+#
+#
+#RIG_CONF="/hive-config/rig.conf"
+#[[ ! -f $RIG_CONF ]] && exit 1
+#
+#. $RIG_CONF
+
+
+
+while true; do
+	/hive/opt/ohgodapill/OhGodAnETHlargementPill-r2 > /var/run/hive/ohgodapill 2>&1
+	sleep 1
+done
+
+
+#exit 0
