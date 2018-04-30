d65be15671f2850cea96204714418e4329f59414
Игорь <igor.villos@gmail.com>
2018-04-30 20:39:54 +0300

    Fix3

--- hive/opt/tabletka/start.sh
+++ hive/opt/tabletka/start.sh
@@ -7,14 +7,14 @@ RIG_CONF="/hive-config/rig.conf"
 
 . $RIG_CONF
 
-if [  $TABLETKA -eq 1 ];then
+
 
 while true 
 do
 
-/hive/opt/tabletka/OhGodAnETHlargementPill-r2 > /var/run/hive/tabletka 
+/hive/opt/tabletka/OhGodAnETHlargementPill-r2 > /var/run/hive/OHGODAPILL 
 sleep 3
 done
 
-fi
+
 #exit 0
