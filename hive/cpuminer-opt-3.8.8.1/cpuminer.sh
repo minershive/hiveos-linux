c2b36105a37b69a95ed0a13327d11aa611e82f60
HaloGenius <halogenius@yandex.ru>
2018-06-03 01:26:21 +0300

    Update cpuminer-opt to v3.8.8.1 - fix run on AMD Ryzen CPU (improve optimization) - fix run on 2nd generation Intel Core CPU

--- hive/cpuminer-opt-3.8.8.1/cpuminer.sh
+++ hive/cpuminer-opt-3.8.8.1/cpuminer.sh
@@ -10,7 +10,7 @@ FLAG_SSE42=`echo ${CPU_INFO} | grep -c " sse4_2 "`
 FLAG_AVX=`echo ${CPU_INFO} | grep -c " avx "`
 FLAG_AES=`echo ${CPU_INFO} | grep -c " aes "`
 FLAG_AVX2=`echo ${CPU_INFO} | grep -c " avx2 "`
-FLAG_SHA=`echo ${CPU_INFO} | grep -c " sha "`
+FLAG_SHA=`echo ${CPU_INFO} | grep -c " sha_ni "`
 
 #echo SSE2:$FLAG_SSE2 SSE42:$FLAG_SSE42 AVX:$FLAG_AVX AES:$FLAG_AES AVX2:$FLAG_AVX2 SHA:$FLAG_SHA
 
@@ -19,7 +19,7 @@ if [[ $FLAG_SSE2 == 1 && $FLAG_SSE42 == 1 && $FLAG_AVX == 0 && $FLAG_AES == 0 &&
     CPU_FLAG="sse42"
 elif [[ $FLAG_SSE2 == 1 && $FLAG_SSE42 == 1 && $FLAG_AVX == 1 && $FLAG_AES == 0 && $FLAG_AVX2 == 0 ]]; then
     CPU_FLAG="avx"
-elif [[ $FLAG_SSE2 == 1 && $FLAG_SSE42 == 1 && $FLAG_AVX == 1 && $FLAG_AES == 1 && $FLAG_AVX2 != 0 ]]; then
+elif [[ $FLAG_SSE2 == 1 && $FLAG_SSE42 == 1 && $FLAG_AVX == 1 && $FLAG_AES == 1 && $FLAG_AVX2 == 0 ]]; then
     CPU_FLAG="aes-avx"
 elif [[ $FLAG_SSE2 == 1 && $FLAG_SSE42 == 1 && $FLAG_AVX == 0 && $FLAG_AES == 1 && $FLAG_AVX2 == 0 ]]; then
     CPU_FLAG="aes-sse42"
@@ -30,7 +30,6 @@ elif [[ $FLAG_AVX2 == 1 ]]; then
 else
 	CPU_FLAG="sse2"
 fi
-
 
 
 echo -e "Running ${CYAN}cpuminer-opt${NOCOLOR} optimized for ${GREEN}$CPU_FLAG${NOCOLOR}" | tee /var/log/miner/cpuminer-opt/cpuminer-opt.log
