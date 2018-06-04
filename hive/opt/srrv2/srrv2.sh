282b3b57349f14ebf3b61f3b996b52aae7e764fa
root <49.pasha@gmail.com>
2018-06-04 16:59:28 +0300

    SRRv2 Extension Boards

--- hive/opt/srrv2/srrv2.sh
+++ hive/opt/srrv2/srrv2.sh
@@ -14,10 +14,11 @@ if [[ ! -z $PREVPID ]]; then
 	kill -9 $PREVPID
 fi
 
+EB_SERIAL=""
 SRRV2_CONF="/hive-config/watchdog_srrv2.txt"
 [ ! -e $SRRV2_CONF ] && echo "Not found config $SRRV2_CONF" && exit 1
 eval "`cat $SRRV2_CONF | dos2unix`"
 [ $ENABLE != "1" ] && echo "srrv2 disabled" && exit 1
 
 path=$(dirname $(realpath $0))
-nohup $path/srrv2 $SERIAL_NUMBER $SLOT_NUMBER > /dev/null 2>&1 &
+nohup $path/srrv2 $SERIAL_NUMBER $SLOT_NUMBER $EB_SERIAL > /dev/null 2>&1 &
