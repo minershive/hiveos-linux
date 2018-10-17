fcd84beb8904616b9d051cb8f3891b7bc22541f1
Fern <fern@kgroup.biz>
2018-10-18 00:41:42 +0300

    minor

--- hive/miners/xmr-stak/h-config.sh
+++ hive/miners/xmr-stak/h-config.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-
-# Not required
 function miner_fork() {
 	local MINER_FORK=$XMR_STAK_FORK
 	[[ -z $MINER_FORK ]] && MINER_FORK=$MINER_DEFAULT_FORK
 
 	echo $MINER_FORK
 }
+
 
 function miner_ver() {
 	if [[ $MINER_FORK == "fireice-uk" ]]; then
@@ -17,15 +16,17 @@ function miner_ver() {
 	fi
 }
 
+
 function miner_config_echo() {
 	export MINER_FORK=`miner_fork`
 	local MINER_VER=`miner_ver`
-	miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/config.txt"
-	miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/pools.txt"
-	miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/amd.txt"
-	miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/nvidia.txt"
-	miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/cpu.txt"
+	miner_echo_config_file "$MINER_DIR/$MINER_FORK/$MINER_VER/config.txt"
+	miner_echo_config_file "$MINER_DIR/$MINER_FORK/$MINER_VER/pools.txt"
+	miner_echo_config_file "$MINER_DIR/$MINER_FORK/$MINER_VER/amd.txt"
+	miner_echo_config_file "$MINER_DIR/$MINER_FORK/$MINER_VER/nvidia.txt"
+	miner_echo_config_file "$MINER_DIR/$MINER_FORK/$MINER_VER/cpu.txt"
 }
+
 
 function miner_config_gen() {
 	[[ -z $XMR_STAK_PASS ]] && XMR_STAK_PASS="x"
@@ -45,106 +46,106 @@ function miner_config_gen() {
 	local CPU_CONFIG="$MINER_DIR/$MINER_FORK/$MINER_VER/cpu.txt"
 	mkfile_from_symlink $CPU_CONFIG
 
-conf=`cat $MINER_DIR/$MINER_FORK/$MINER_VER/config_global.json`
+	conf=`cat $MINER_DIR/$MINER_FORK/$MINER_VER/config_global.json`
 
-currency=""
-case `jq -r '."xmr-stak".coin' <<< $META` in
-	AEON )
-		currency='"currency" : "aeon7"'
-	;;
-	BBS )
-		currency='"currency" : "bbscoin"'
-	;;
-	CROAT )
-		currency='"currency" : "croat"'
-	;;
-	EDL )
-		currency='"currency" : "edollar"'
-	;;
-	ETN )
-		currency='"currency" : "electroneum"'
-	;;
-	GRFT )
-		currency='"currency" : "graft"'
-	;;
-	XHV )
-		currency='"currency" : "haven"'
-	;;
-	ITNS )
-		currency='"currency" : "intense"'
-	;;
-	KRB )
-		currency='"currency" : "karbo"'
-	;;
-	XMR )
-		currency='"currency" : "monero7"'
-	;;
-	XTL )
-		currency='"currency" : "stellite"'
-	;;
-	SUMO )
-		currency='"currency" : "sumokoin"'
-	;;
-	TRTL )
-		currency='"currency" : "turtlecoin"'
-	;;
-	# * )
-	# 	currency='"currency" : "'$XMR_STAK_ALGO'"'
-	# ;;
-esac
-[[ ! -z $currency ]] && conf=$(jq -s '.[0] * .[1]' <<< "$conf {$currency}")
+	currency=""
+	case `jq -r '."xmr-stak".coin' <<< $META` in
+		AEON )
+			currency='"currency" : "aeon7"'
+		;;
+		BBS )
+			currency='"currency" : "bbscoin"'
+		;;
+		CROAT )
+			currency='"currency" : "croat"'
+		;;
+		EDL )
+			currency='"currency" : "edollar"'
+		;;
+		ETN )
+			currency='"currency" : "electroneum"'
+		;;
+		GRFT )
+			currency='"currency" : "graft"'
+		;;
+		XHV )
+			currency='"currency" : "haven"'
+		;;
+		ITNS )
+			currency='"currency" : "intense"'
+		;;
+		KRB )
+			currency='"currency" : "karbo"'
+		;;
+		XMR )
+			currency='"currency" : "monero7"'
+		;;
+		XTL )
+			currency='"currency" : "stellite"'
+		;;
+		SUMO )
+			currency='"currency" : "sumokoin"'
+		;;
+		TRTL )
+			currency='"currency" : "turtlecoin"'
+		;;
+		# * )
+		# 	currency='"currency" : "'$XMR_STAK_ALGO'"'
+		# ;;
+	esac
+	[[ ! -z $currency ]] && conf=$(jq -s '.[0] * .[1]' <<< "$conf {$currency}")
 
-#merge user config options into main config
-if [[ ! -z $XMR_STAK_USER_CONFIG ]]; then
-	while read -r line; do
-		[[ -z $line ]] && continue
-		conf=$(jq -s '.[0] * .[1]' <<< "$conf {$line}")
-	done <<< "$XMR_STAK_USER_CONFIG"
-fi
+	#merge user config options into main config
+	if [[ ! -z $XMR_STAK_USER_CONFIG ]]; then
+		while read -r line; do
+			[[ -z $line ]] && continue
+			conf=$(jq -s '.[0] * .[1]' <<< "$conf {$line}")
+		done <<< "$XMR_STAK_USER_CONFIG"
+	fi
 
-pools='[]'
-#this is undocumented, but we will use this own feature
-use_tls=$(jq -r .use_tls <<< "$conf")
-[[ -z $use_tls || $use_tls == "null" ]] && use_tls="false"
+	pools='[]'
+	#this is undocumented, but we will use this own feature
+	use_tls=$(jq -r .use_tls <<< "$conf")
+	[[ -z $use_tls || $use_tls == "null" ]] && use_tls="false"
 
-for url in $XMR_STAK_URL
-do
-	grep -q "nicehash.com" <<< $XMR_STAK_URL
-	[[ $? -eq 0 ]] && nicehash="true" || nicehash="false"
-	pool=$(cat <<EOF
-			{"pool_address": "$url", "wallet_address": "$XMR_STAK_TEMPLATE", "pool_password": "$XMR_STAK_PASS", "use_nicehash": $nicehash, "use_tls": $use_tls, "tls_fingerprint": "", "pool_weight": 1, "rig_id": "$WORKER_NAME" }
+	for url in $XMR_STAK_URL
+	do
+		grep -q "nicehash.com" <<< $XMR_STAK_URL
+		[[ $? -eq 0 ]] && nicehash="true" || nicehash="false"
+		pool=$(cat <<EOF
+				{"pool_address": "$url", "wallet_address": "$XMR_STAK_TEMPLATE", "pool_password": "$XMR_STAK_PASS", "use_nicehash": $nicehash, "use_tls": $use_tls, "tls_fingerprint": "", "pool_weight": 1, "rig_id": "$WORKER_NAME" }
 EOF
-)
-	pools=`jq --null-input --argjson pools "$pools" --argjson pool "$pool" '$pools + [$pool]'`
-done
+	)
+		pools=`jq --null-input --argjson pools "$pools" --argjson pool "$pool" '$pools + [$pool]'`
+	done
 
-if [[ -z $pools || $pools == '[]' || $pools == 'null' ]]; then
-	echo -e "${RED}No pools configured, using default${NOCOLOR}"
-else
-	#Don't remove until Hive 1 is gone
-	#pass can also contain %var%
-	[[ ! -z $EWAL ]] && pools=${pools/\%EWAL\%/$EWAL}
-	[[ ! -z $ZWAL ]] && pools=${pools/\%ZWAL\%/$ZWAL}
-	[[ ! -z $DWAL ]] && pools=${pools/\%DWAL\%/$DWAL}
-	[[ ! -z $EMAIL ]] && pools=${pools/\%EMAIL\%/$EMAIL}
-	[[ ! -z $WORKER_NAME ]] && pools=${pools/\%WORKER_NAME\%/$WORKER_NAME} || echo -e "${YELLOW}WORKER_NAME not set${NOCOLOR}";
-	[[ ! -z $XMR_STAK_ALGO ]] && pools=${pools/\%XMR_STAK_ALGO\%/$XMR_STAK_ALGO}
-	[[ ! -z $MINER_API_PORT ]] && pools=${pools/\%MINER_API_PORT\%/$MINER_API_PORT}
+	if [[ -z $pools || $pools == '[]' || $pools == 'null' ]]; then
+		echo -e "${RED}No pools configured, using default${NOCOLOR}"
+	else
+		#Don't remove until Hive 1 is gone
+		#pass can also contain %var%
+		[[ ! -z $EWAL ]] && pools=${pools/\%EWAL\%/$EWAL}
+		[[ ! -z $ZWAL ]] && pools=${pools/\%ZWAL\%/$ZWAL}
+		[[ ! -z $DWAL ]] && pools=${pools/\%DWAL\%/$DWAL}
+		[[ ! -z $EMAIL ]] && pools=${pools/\%EMAIL\%/$EMAIL}
+		[[ ! -z $WORKER_NAME ]] && pools=${pools/\%WORKER_NAME\%/$WORKER_NAME} || echo -e "${YELLOW}WORKER_NAME not set${NOCOLOR}";
+		[[ ! -z $XMR_STAK_ALGO ]] && pools=${pools/\%XMR_STAK_ALGO\%/$XMR_STAK_ALGO}
+		[[ ! -z $MINER_API_PORT ]] && pools=${pools/\%MINER_API_PORT\%/$MINER_API_PORT}
 
-	pools=`jq --null-input --argjson pool_list "$pools" '{$pool_list}'`
-	conf=$(jq -s '.[0] * .[1]' <<< "$conf $pools")
-fi
+		pools=`jq --null-input --argjson pool_list "$pools" '{$pool_list}'`
+		conf=$(jq -s '.[0] * .[1]' <<< "$conf $pools")
+	fi
 
 
-#amd nvidia cpu overrides or default
-#[[ ! -z $XMR_STAK__AMD ]] &&
-echo "$XMR_STAK_AMD" > $AMD_CONFIG
-#[[ ! -z $XMR_STAK__NVIDIA ]] &&
-echo "$XMR_STAK_NVIDIA" > $NVIDIA_CONFIG
-#[[ ! -z $XMR_STAK__CPU ]] &&
-echo "$XMR_STAK_CPU" > $CPU_CONFIG
+	#amd nvidia cpu overrides or default
+	#[[ ! -z $XMR_STAK__AMD ]] &&
+	echo "$XMR_STAK_AMD" > $AMD_CONFIG
+	#[[ ! -z $XMR_STAK__NVIDIA ]] &&
+	echo "$XMR_STAK_NVIDIA" > $NVIDIA_CONFIG
+	#[[ ! -z $XMR_STAK__CPU ]] &&
+	echo "$XMR_STAK_CPU" > $CPU_CONFIG
 
-#delete { and } lines
-echo $conf | jq . | sed 1d | sed '$d' > $MINER_CONFIG
-echo $conf | jq . | sed 1d | sed '$d' > $POOLS_CONFIG
+	#delete { and } lines
+	echo $conf | jq . | sed 1d | sed '$d' > $MINER_CONFIG
+	echo $conf | jq . | sed 1d | sed '$d' > $POOLS_CONFIG
 }
--- hive/miners/xmr-stak/h-stats.sh
+++ hive/miners/xmr-stak/h-stats.sh
@@ -1,42 +1,40 @@
 #!/usr/bin/env bash
 
-  MINER_LOG_BASENAME="/var/log/miner/xmr-stak/xmr-stak"
+stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:${MINER_API_PORT}/api.json`
+if [[ $? -ne 0 || -z $stats_raw ]]; then
+	echo -e "${YELLOW}Failed to read $miner from localhost:${MINER_API_PORT}${NOCOLOR}"
+else
+	# [[ -z $XMR_STAK_ALGO ]] && XMR_STAK_ALGO="cryptonight"
 
-	stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:${MINER_API_PORT}/api.json`
-	if [[ $? -ne 0 || -z $stats_raw ]]; then
-		echo -e "${YELLOW}Failed to read $miner from localhost:${MINER_API_PORT}${NOCOLOR}"
+	khs=`echo $stats_raw | jq -r '.hashrate.total[0]' | awk '{print $1/1000}'`
+
+	local cpu_temp=`cat /sys/class/hwmon/hwmon0/temp*_input | head -n $(nproc) | awk '{print $1/1000}' | jq -rsc .` #just a try to get CPU temps
+
+	local gpus_disabled=
+			(head -n 40 ${MINER_LOG_BASENAME}.log | grep -q "WARNING: backend AMD (OpenCL) disabled") && #AMD disabled found
+			(head -n 40 ${MINER_LOG_BASENAME}.log | grep -q "WARNING: backend NVIDIA disabled") && #and nvidia disabled
+			gpus_disabled=1
+	if [[ $gpus_disabled == 1 ]]; then #gpus disabled
+		local temp='[]'
+		local fan='[]'
 	else
-		# [[ -z $XMR_STAK_ALGO ]] && XMR_STAK_ALGO="cryptonight"
-
-		khs=`echo $stats_raw | jq -r '.hashrate.total[0]' | awk '{print $1/1000}'`
-
-		local cpu_temp=`cat /sys/class/hwmon/hwmon0/temp*_input | head -n $(nproc) | awk '{print $1/1000}' | jq -rsc .` #just a try to get CPU temps
-
-		local gpus_disabled=
-				(head -n 40 ${MINER_LOG_BASENAME}.log | grep -q "WARNING: backend AMD (OpenCL) disabled") && #AMD disabled found
-				(head -n 40 ${MINER_LOG_BASENAME}.log | grep -q "WARNING: backend NVIDIA disabled") && #and nvidia disabled
-				gpus_disabled=1
-		if [[ $gpus_disabled == 1 ]]; then #gpus disabled
-			local temp='[]'
-			local fan='[]'
-		else
-			local temp=$(jq '.temp' <<< $gpu_stats)
-			local fan=$(jq '.fan' <<< $gpu_stats)
-			[[ $cpu_indexes_array != '[]' ]] && #remove Internal Gpus
-				temp=$(jq -c "del(.$cpu_indexes_array)" <<< $temp) &&
-				fan=$(jq -c "del(.$cpu_indexes_array)" <<< $fan)
-		fi
-
-		local ac=$(jq '.results.shares_good' <<< "$stats_raw")
-		local rj=$(( $(jq '.results.shares_total' <<< "$stats_raw") - $ac ))
-
-		local algo=`cat /run/hive/miners/xmr-stak/config.txt | grep -m1 '"currency"' | sed -E 's/\s*".*":\s*"(.*)",/\1/g'`
-
-		stats=$(jq --argjson temp "$temp" --argjson fan "$fan" \
-					--argjson cpu_temp "$cpu_temp" --arg ac "$ac" --arg rj "$rj" \
-					--arg algo "$algo" \
-			'{hs: [.hashrate.threads[][0]], $algo, $temp, $fan, $cpu_temp, uptime: .connection.uptime, ar: [$ac, $rj]}' <<< "$stats_raw")
+		local temp=$(jq '.temp' <<< $gpu_stats)
+		local fan=$(jq '.fan' <<< $gpu_stats)
+		[[ $cpu_indexes_array != '[]' ]] && #remove Internal Gpus
+			temp=$(jq -c "del(.$cpu_indexes_array)" <<< $temp) &&
+			fan=$(jq -c "del(.$cpu_indexes_array)" <<< $fan)
 	fi
 
-	[[ -z $khs ]] && khs=0
-	[[ -z $stats ]] && stats="null"
+	local ac=$(jq '.results.shares_good' <<< "$stats_raw")
+	local rj=$(( $(jq '.results.shares_total' <<< "$stats_raw") - $ac ))
+
+	local algo=`cat /run/hive/miners/xmr-stak/config.txt | grep -m1 '"currency"' | sed -E 's/\s*".*":\s*"(.*)",/\1/g'`
+
+	stats=$(jq --argjson temp "$temp" --argjson fan "$fan" \
+				--argjson cpu_temp "$cpu_temp" --arg ac "$ac" --arg rj "$rj" \
+				--arg algo "$algo" \
+		'{hs: [.hashrate.threads[][0]], $algo, $temp, $fan, $cpu_temp, uptime: .connection.uptime, ar: [$ac, $rj]}' <<< "$stats_raw")
+fi
+
+[[ -z $khs ]] && khs=0
+[[ -z $stats ]] && stats="null"
