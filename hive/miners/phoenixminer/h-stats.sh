0add1e37df872b3883666bee25254f2b3655deb5
Fern <fern@kgroup.biz>
2018-11-04 04:57:17 +0200

    phoenix removed unused dep

--- hive/miners/phoenixminer/h-stats.sh
+++ hive/miners/phoenixminer/h-stats.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-#######################
-# MAIN script body
-#######################
-
-. /hive/custom/phoenixminer/h-manifest.conf
-
 stats_raw=`echo '{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}' | nc -w $API_TIMEOUT localhost ${MINER_API_PORT} | jq '.result'`
 if [[ $? -ne 0  || -z $stats_raw ]]; then
 	echo -e "${YELLOW}Failed to read $miner stats_raw from localhost:${MINER_API_PORT} ${NOCOLOR}"
