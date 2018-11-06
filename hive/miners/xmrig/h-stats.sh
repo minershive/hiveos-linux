8c5ffc2661cb1b2a809509cc3e8878966b32b11b
HaloGenius <halogenius@yandex.ru>
2018-11-06 02:56:53 +0200

    Add A/R for XMRig

--- hive/miners/xmrig/h-stats.sh
+++ hive/miners/xmrig/h-stats.sh
@@ -6,6 +6,11 @@ if [[ $? -ne 0 || -z $stats_raw ]]; then
 	echo -e "${YELLOW}Failed to read $miner from localhost:$MINER_API_PORT${NOCOLOR}"
 else
 	khs=`echo $stats_raw | jq -r '.hashrate.total[0]' | awk '{print $1/1000}'`
+	local ac=$(jq '.results.shares_good' <<< "$stats_raw")
+	local rj=$(( $(jq '.results.shares_total' <<< "$stats_raw") - $ac ))
 	local cpu_temp=`cat /sys/class/hwmon/hwmon0/temp*_input | head -n $(nproc) | awk '{print $1/1000}' | jq -rsc .` #just a try to get CPU temps
-	stats=`echo $stats_raw | jq '{hs: [.hashrate.threads[][0]], temp: '$cpu_temp', uptime: .connection.uptime, algo: .algo}'`
+	stats=`echo $stats_raw | jq --arg ac "$ac" --arg rj "$rj" '{hs: [.hashrate.threads[][0]], temp: '$cpu_temp', ar: [$ac, $rj], uptime: .connection.uptime, algo: .algo}'`
 fi
+
+[[ -z $khs ]] && khs=0
+[[ -z $stats ]] && stats="null"
