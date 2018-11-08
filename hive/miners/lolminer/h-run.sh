2f36529fbe2392e8fa7f339e8caa3dd462c0d1a4
HaloGenius <halogenius@yandex.ru>
2018-11-08 03:20:21 +0200

    Fixed lolMiner scripts

--- hive/miners/lolminer/h-run.sh
+++ hive/miners/lolminer/h-run.sh
@@ -4,4 +4,4 @@ cd $MINER_DIR/$MINER_VER
 
 [[ ! -e ./lolminer.conf ]] && echo "No config file found, exiting" && exit 1
 
-lolMiner $(< lolminer.conf) $@ 2>&1 | tee $MINER_LOG_BASENAME.log
+lolMiner $(< lolminer.conf) | tee $MINER_LOG_BASENAME.log
--- hive/miners/lolminer/h-stats.sh
+++ hive/miners/lolminer/h-stats.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 
 
-#######################
-# Functions
-#######################
-
-get_amd_cards_temp(){
-	echo $(jq -c "[.temp$amd_indexes_array]" <<< $gpu_stats)
-}
-
-get_amd_cards_fan(){
-	echo $(jq -c "[.fan$amd_indexes_array]" <<< $gpu_stats)
-}
-
-#######################
-# MAIN script body
-#######################
 stats_raw=`curl --connect-timeout 2 --max-time ${API_TIMEOUT} --silent --noproxy '*' http://127.0.0.1:${MINER_API_PORT}/summary`
 if [[ $? -ne 0 || -z $stats_raw ]]; then
 	echo -e "${YELLOW}Failed to read $miner from localhost:${MINER_API_PORT}${NOCOLOR}"
@@ -24,8 +9,8 @@ else
 	local fan=$(jq -c "[.fan$amd_indexes_array]" <<< $gpu_stats)
 	local temp=$(jq -c "[.temp$amd_indexes_array]" <<< $gpu_stats)
 	stats=$(jq 	--argjson temp "$temp" \
-				--argjson fan "$fan" \
-				'{hs: [.GPUs[].Performance], hs_units: "hs", $temp, $fan, uptime: .Session.Uptime, ar: [.Session.Accepted, .Session.Submitted - .Session.Accepted ], algo: .Mining.Algorithm}' <<< "$stats_raw")
+			--argjson fan "$fan" \
+			'{hs: [.GPUs[].Performance], hs_units: "hs", $temp, $fan, uptime: .Session.Uptime, ar: [.Session.Accepted, .Session.Submitted - .Session.Accepted ], algo: .Mining.Algorithm}' <<< "$stats_raw")
 fi
 
 [[ -z $khs ]] && khs=0
