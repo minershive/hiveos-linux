73165fcdffa07eb9071c96a2717cab0d49cfd0b2
Fern <fern@kgroup.biz>
2018-11-08 18:53:23 +0800

    preclone

--- hive/miners/t-rex/h-stats.sh
+++ hive/miners/t-rex/h-stats.sh
@@ -5,22 +5,22 @@
 stat_raw=`echo 'summary' | nc -w $API_TIMEOUT localhost $MINER_API_PORT`
 
 if [[ $? -ne 0 ]]; then
-        echo -e "${YELLOW}Failed to read miner stats from localhost:${MINER_API_PORT}${NOCOLOR}"
+	echo -e "${YELLOW}Failed to read miner stats from localhost:${MINER_API_PORT}${NOCOLOR}"
 else
-        local gpu_worked=`echo $stat_raw | jq '.gpus[].gpu_id'`
-        local gpu_busid=(`cat /var/run/hive/gpu-detect.json | jq -r '.[] | select(.brand=="nvidia") | .busid' | cut -d ':' -f 1`)
-        local busids=''
-        local idx=0
-        for i in $gpu_worked; do
-                gpu=${gpu_busid[$i]}
-                busids[idx]=$((16#$gpu))
-                idx=$((idx+1))
-        done
-        stats=$(jq --argjson gpus `echo ${busids[@]} | jq -cs '.'` \
-		'{hs: [.gpus[].hashrate], hs_units: "hs", temp: [.gpus[].temperature], fan: [.gpus[].fan_speed], uptime: .uptime, ar: [.accepted_count, .rejected_count], bus_numbers: $gpus, algo: .algorithm}' <<< $stat_raw)
+	local gpu_worked=`echo $stat_raw | jq '.gpus[].gpu_id'`
+	local gpu_busid=(`cat /var/run/hive/gpu-detect.json | jq -r '.[] | select(.brand=="nvidia") | .busid' | cut -d ':' -f 1`)
+	local busids=''
+	local idx=0
+	for i in $gpu_worked; do
+		gpu=${gpu_busid[$i]}
+		busids[idx]=$((16#$gpu))
+		idx=$((idx+1))
+	done
+	stats=$(jq --argjson gpus `echo ${busids[@]} | jq -cs '.'` \
+	'{hs: [.gpus[].hashrate], hs_units: "hs", temp: [.gpus[].temperature], fan: [.gpus[].fan_speed], uptime: .uptime, ar: [.accepted_count, .rejected_count], bus_numbers: $gpus, algo: .algorithm}' <<< $stat_raw)
 
-        # total hashrate in khs
-        khs=$(jq ".hashrate/1000" <<< $stat_raw)
+	# total hashrate in khs
+	khs=$(jq ".hashrate/1000" <<< $stat_raw)
 fi
 
 [[ -z $khs ]] && khs=0
