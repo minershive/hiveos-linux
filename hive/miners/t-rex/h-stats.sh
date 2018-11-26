60df9f0d4d277424a02c825ab82fe9c47d264397
HaloGenius <halogenius@yandex.ru>
2018-11-26 10:05:58 +0200

    Updated T-Rex to 0.8.1 (x22i +20%)

--- hive/miners/t-rex/h-stats.sh
+++ hive/miners/t-rex/h-stats.sh
@@ -17,7 +17,7 @@ else
 		idx=$((idx+1))
 	done
 	stats=$(jq --argjson gpus `echo ${busids[@]} | jq -cs '.'` \
-	'{hs: [.gpus[].hashrate], hs_units: "hs", temp: [.gpus[].temperature], fan: [.gpus[].fan_speed], uptime: .uptime, ar: [.accepted_count, .rejected_count], bus_numbers: $gpus, algo: .algorithm}' <<< $stat_raw)
+	'{ver: .version, hs: [.gpus[].hashrate], hs_units: "hs", temp: [.gpus[].temperature], fan: [.gpus[].fan_speed], uptime: .uptime, ar: [.accepted_count, .rejected_count], bus_numbers: $gpus, algo: .algorithm}' <<< $stat_raw)
 
 	# total hashrate in khs
 	khs=$(jq ".hashrate/1000" <<< $stat_raw)
