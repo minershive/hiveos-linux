a045f489b248691ef4e19ac613a69cf2e684f735
atrump <trump.andrey@gmail.com>
2018-12-30 17:18:07 +0300

    gminer 1.13: new algorithm "equihash 96/5"; add failover pools.

--- hive/miners/gminer/h-config.sh
+++ hive/miners/gminer/h-config.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
 
 function miner_ver() {
-	local MINER_VER=$GMINER_VER
-	[[ -z $MINER_VER ]] && MINER_VER=$MINER_LATEST_VER
-	echo $MINER_VER
+  local MINER_VER=$GMINER_VER
+  [[ -z $MINER_VER ]] && MINER_VER=$MINER_LATEST_VER
+  echo $MINER_VER
 }
 
 
 function miner_config_echo() {
-	local MINER_VER=`miner_ver`
-	miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/$MINER_NAME.conf"
+  local MINER_VER=`miner_ver`
+  miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/$MINER_NAME.conf"
 }
 
 function miner_config_gen() {
-	local MINER_CONFIG="$MINER_DIR/$MINER_VER/$MINER_NAME.conf"
-	mkfile_from_symlink $MINER_CONFIG
+  local MINER_CONFIG="$MINER_DIR/$MINER_VER/$MINER_NAME.conf"
+  mkfile_from_symlink $MINER_CONFIG
 
-	[[ -z $GMINER_ALGO ]] && GMINER_ALGO="144_5"
-	local host=`head -n 1 <<< "$GMINER_HOST"`
-	local port=`head -n 1 <<< "$GMINER_PORT"`
+  [[ -z $GMINER_ALGO ]] && GMINER_ALGO="144_5"
+  local conf="--algo $GMINER_ALGO"
 
-	local conf="--algo $GMINER_ALGO --server $host --port $port --user $GMINER_TEMPLATE --pass $GMINER_PASS --pec $GMINER_USER_CONFIG"
+  local host=
+  local port=
 
-	echo "$conf" > $MINER_CONFIG
+  for (( i = 1; i <= `wc -w <<< $GMINER_HOST`; i++ )); do
+    host=`awk '(NR == '$i')' <<< "$GMINER_HOST"`
+    port=`awk '(NR == '$i')' <<< "$GMINER_PORT"`
+
+    conf+=" --server $host --port $port --user $GMINER_TEMPLATE --pass $GMINER_PASS"
+  done
+
+  conf+=" --pec $GMINER_USER_CONFIG"
+
+  echo "$conf" > $MINER_CONFIG
 }
--- hive/miners/gminer/h-stats.sh
+++ hive/miners/gminer/h-stats.sh
@@ -2,44 +2,44 @@
 
 stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:$MINER_API_PORT/stat`
 if [[ $? -ne 0  || -z $stats_raw ]]; then
-	echo -e "${YELLOW}Failed to read $miner stats_raw from localhost:${MINER_API_PORT}${NOCOLOR}"
+  echo -e "${YELLOW}Failed to read $miner stats_raw from localhost:${MINER_API_PORT}${NOCOLOR}"
 else
-	khs=`echo $stats_raw | jq -r '.devices[].speed' | awk '{s+=$1} END {print s/1000}'` #sum up and convert to khs
-	local ac=$(jq '[.devices[].accepted_shares] | add' <<< "$stats_raw")
-	local rj=$(jq '[.devices[].rejected_shares] | add' <<< "$stats_raw")
+  khs=`echo $stats_raw | jq -r '.devices[].speed' | awk '{s+=$1} END {print s/1000}'` #sum up and convert to khs
+  local ac=$(jq '[.devices[].accepted_shares] | add' <<< "$stats_raw")
+  local rj=$(jq '[.devices[].rejected_shares] | add' <<< "$stats_raw")
 
-	#All fans speed array
-	local fan=$(jq -r ".fan | .[]" <<< $gpu_stats)
-	#gminer's busid array
-	local bus_id_array=$(jq -r '[.devices[].bus_id[5:7]] | .[]' <<< "$stats_raw")
-	local bus_numbers=$(echo "$bus_id_array" | awk '{printf("%d\n", "0x"$1)}' | jq -sc .)
-	#All busid array
-	local all_bus_ids_array=(`echo "$gpu_detect_json" | jq -r '[ . | to_entries[] | select(.value) | .value.busid [0:2] ] | .[]'`)
-	#Formating arrays
-	bus_id_array=`sed 's/\n/ /' <<< $bus_id_array`
-	fan=`sed 's/\n/ /' <<< $fan`
-	IFS=' ' read -r -a bus_id_array <<< "$bus_id_array"
-	IFS=' ' read -r -a fan <<< "$fan"
-	#busid equality
-	local fans_array=
-	for ((i = 0; i < ${#all_bus_ids_array[@]}; i++)); do
-		for ((j = 0; j < ${#bus_id_array[@]}; j++)); do
-			if [[ "$(( 0x${all_bus_ids_array[$i]} ))" -eq "$(( 0x${bus_id_array[$j]} ))" ]]; then
-				fans_array+=("${fan[$i]}")
-			fi
-		done
-	done
+  #All fans speed array
+  local fan=$(jq -r ".fan | .[]" <<< $gpu_stats)
+  #gminer's busid array
+  local bus_id_array=$(jq -r '[.devices[].bus_id[5:7]] | .[]' <<< "$stats_raw")
+  local bus_numbers=$(echo "$bus_id_array" | awk '{printf("%d\n", "0x"$1)}' | jq -sc .)
+  #All busid array
+  local all_bus_ids_array=(`echo "$gpu_detect_json" | jq -r '[ . | to_entries[] | select(.value) | .value.busid [0:2] ] | .[]'`)
+  #Formating arrays
+  bus_id_array=`sed 's/\n/ /' <<< $bus_id_array`
+  fan=`sed 's/\n/ /' <<< $fan`
+  IFS=' ' read -r -a bus_id_array <<< "$bus_id_array"
+  IFS=' ' read -r -a fan <<< "$fan"
+  #busid equality
+  local fans_array=
+  for ((i = 0; i < ${#all_bus_ids_array[@]}; i++)); do
+    for ((j = 0; j < ${#bus_id_array[@]}; j++)); do
+      if [[ "$(( 0x${all_bus_ids_array[$i]} ))" -eq "$(( 0x${bus_id_array[$j]} ))" ]]; then
+        fans_array+=("${fan[$i]}")
+      fi
+    done
+  done
 
-		[[ -z $GMINER_ALGO ]] && GMINER_ALGO="144_5"
+  [[ -z $GMINER_ALGO ]] && GMINER_ALGO="144_5"
 
-	local temp=$(jq -c "[.temp$nvidia_indexes_array]" <<< $gpu_stats)
+  local temp=$(jq -c "[.temp$nvidia_indexes_array]" <<< $gpu_stats)
 
-	stats=$(jq -c --argjson temp "$temp" --argjson fan "`echo "${fans_array[@]}" | jq -s . | jq -c .`" \
-			--arg ac "$ac" --arg rj "$rj" \
-			--argjson bus_numbers "$bus_numbers" --arg algo "$GMINER_ALGO"  \
-			--arg ver `miner_ver` \
-		'{hs: [.devices[].speed], hs_units: "hs", $temp, $fan,
-			uptime: .uptime, ar: [$ac, $rj], $bus_numbers, $algo, $ver}' <<< "$stats_raw")
+  stats=$(jq -c --argjson temp "$temp" --argjson fan "`echo "${fans_array[@]}" | jq -s . | jq -c .`" \
+        --arg ac "$ac" --arg rj "$rj" \
+        --argjson bus_numbers "$bus_numbers" --arg algo "$GMINER_ALGO"  \
+        --arg ver `miner_ver` \
+        '{hs: [.devices[].speed], hs_units: "hs", $temp, $fan,
+        uptime: .uptime, ar: [$ac, $rj], $bus_numbers, $algo, $ver}' <<< "$stats_raw")
 fi
 
 # {
