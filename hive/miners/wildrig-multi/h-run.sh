4763a028f421137c60716138cc966a80ea29fd8c
HaloGenius <halogenius@yandex.ru>
2019-01-03 22:46:53 +0200

    wildrig-multi 0.15.0 * added x16rt and mtp algos * wildrig-multi 0.15.0.9 build

--- hive/miners/wildrig-multi/h-run.sh
+++ hive/miners/wildrig-multi/h-run.sh
@@ -3,9 +3,17 @@
 #installing libuv1 if necessary
 #[[ `dpkg -s libuv1 2>/dev/null | grep -c "ok installed"` -eq 0 ]] && apt-get install -y libuv1
 
-[[ `ps aux | grep "./t-rex" | grep -v grep | wc -l` != 0 ]] &&
+[[ `ps aux | grep "./wildrig-multi" | grep -v grep | wc -l` != 0 ]] &&
   echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
   exit 1
+
+if [ $WILDRIG_MULTI_ALGO == "mtp" ]; then
+    export GPU_FORCE_64BIT_PTR=1
+    export GPU_USE_SYNC_OBJECTS=1
+    export GPU_MAX_ALLOC_PERCENT=100
+    export GPU_SINGLE_ALLOC_PERCENT=100
+    export GPU_MAX_HEAP_SIZE=100
+fi
 
 #try to release TIME_WAIT sockets
 while true; do
