07801d7e50cd76666a344853c787cbb4a51cdda8
atrump <trump.andrey@gmail.com>
2019-01-08 01:13:05 +0300

    grinminer 0.5.0

---
+++ hive/miners/grinminer/h-config.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+function miner_ver() {
+	local MINER_VER=$GRINMINER_VER
+	[[ -z $MINER_VER ]] && MINER_VER=$MINER_LATEST_VER
+	echo $MINER_VER
+}
+
+
+function miner_config_echo() {
+	local MINER_VER=`miner_ver`
+	miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/grin-miner.toml"
+}
+
+function miner_config_gen() {
+
+	local MINER_CONFIG="$MINER_DIR/$MINER_VER/grin-miner.toml"
+	mkfile_from_symlink $MINER_CONFIG
+
+	[[ -z $GRINMINER_TEMPLATE ]] && echo -e "${YELLOW}GRINMINER_TEMPLATE is empty${NOCOLOR}" && return 1
+	[[ -z $GRINMINER_URL ]] && echo -e "${YELLOW}GRINMINER_URL is empty${NOCOLOR}" && return 1
+	[[ -z $MINER_CONFIG ]] && echo -e "${RED}No MINER_CONFIG is set${NOCOLOR}" && return 1
+	[[ -z $MINER_LOG_BASENAME ]] && echo -e "${RED}No MINER_LOG_BASENAME is set${NOCOLOR}" && exit 1
+
+	echo "[logging]" > $MINER_CONFIG
+	echo "log_to_stdout = true" >> $MINER_CONFIG
+	echo "stdout_log_level = \"Info\"" >> $MINER_CONFIG
+	echo "log_to_file = true" >> $MINER_CONFIG
+	echo "file_log_level = \"Debug\"" >> $MINER_CONFIG
+	echo "log_file_path = \"${MINER_LOG_BASENAME}.log\"" >> $MINER_CONFIG
+	echo "log_file_append = true" >> $MINER_CONFIG
+
+	echo "[mining]" >> $MINER_CONFIG
+	echo "run_tui = true" >> $MINER_CONFIG
+	local pool=`head -n 1 <<< "$GRINMINER_URL"`
+	echo "stratum_server_addr = \"$pool\"" >> $MINER_CONFIG
+	echo "stratum_server_login = \"$GRINMINER_TEMPLATE\"" >> $MINER_CONFIG
+	echo "stratum_server_password = \"$GRINMINER_PASS\"" >> $MINER_CONFIG
+	echo "stratum_server_tls_enabled = false" >> $MINER_CONFIG
+
+	echo "#####GRINMINER_USER_CONFIG#####" >> $MINER_CONFIG
+
+	while read -r line; do
+    echo $line >> $MINER_CONFIG
+  done <<< "$GRINMINER_USER_CONFIG"
+}
---
+++ hive/miners/grinminer/h-stats.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+#######################
+# Functions
+#######################
+
+get_cards_hashes(){
+#Jan 07 13:01:38.090 DEBG Mining: Plugin 0 - Device 0 (GeForce GTX 1080 Ti) at Cuck(at)oo29 - Status: OK : Last Graph time: 0.206706559s; Graphs per second: 4.838 - Total Attempts: 760
+  hs=''; local t_hs=0
+  local i=0
+  for (( i=0; i < ${GPU_COUNT_NVIDIA}; i++ )); do
+    t_hs=`cat $log_name | tail -n 50 | grep "DEBG Mining:" | grep "Device $i" | tail -n 1`
+    t_hs=`echo ${t_hs#*" Graphs per second: "} | cut -d " " -f 1`
+    hs+=\"$t_hs\"" "
+  done
+}
+
+
+get_total_hashes(){
+#Jan 07 13:01:38.090 INFO Mining: Cuck(at)oo at 4.837775854030834 gps (graphs per second)
+  khs=0
+  khs=`cat $log_name | tail -n 50 | grep "INFO Mining: Cuck(at)oo at" | tail -n 1 | cut -f 8 -d " " -s | awk '{ printf($1/1000) }'`
+}
+
+get_miner_uptime(){
+  local a=0
+  let a=`stat --format='%Y' $log_name`-`stat --format='%Y' $conf_name`
+  echo $a
+}
+
+get_log_time_diff(){
+  local a=0
+  let a=`date +%s`-`stat --format='%Y' $log_name`
+  echo $a
+}
+
+#######################
+# MAIN script body
+#######################
+
+
+local log_name="$MINER_LOG_BASENAME.log"
+local ver=`miner_ver`
+local conf_name="/hive/miners/$MINER_NAME/$ver/grin-miner.toml"
+
+local temp=$(jq -c "[.temp$nvidia_indexes_array]" <<< $gpu_stats)
+local fan=$(jq -c "[.fan$nvidia_indexes_array]" <<< $gpu_stats)
+
+[[ -z $GPU_COUNT_NVIDIA ]] && GPU_COUNT_NVIDIA=`gpu-detect NVIDIA`
+
+# Calc log freshness
+local diffTime=$(get_log_time_diff)
+local maxDelay=120
+
+# echo $diffTime
+
+local algo="cuckoo"
+
+# If log is fresh the calc miner stats or set to null if not
+if [ "$diffTime" -lt "$maxDelay" ]; then
+  get_cards_hashes # hashes, temp, fan array
+  get_total_hashes # total hashes, accepted, rejected
+  local hs_units='hs' # hashes utits
+  local uptime=$(get_miner_uptime) # miner uptime
+
+# make JSON
+#--argjson hs "`echo ${hs[@]} | tr " " "\n" | jq -cs '.'`" \
+  stats=$(jq -nc \
+        --argjson hs "`echo ${hs[@]} | tr " " "\n" | jq -cs '.'`" \
+        --arg hs_units "$hs_units" \
+        --argjson temp "$temp" \
+        --argjson fan "$fan" \
+        --arg uptime "$uptime" \
+        --arg algo "$algo" \
+        --arg ver "$ver" \
+        '{$hs, $hs_units, $temp, $fan, $uptime, $algo, $ver}')
+else
+  stats=""
+  khs=0
+fi
+
+# debug output
+#echo temp:  $temp
+#echo fan:   $fan
+#echo stats: $stats
+#echo khs:   $khs
