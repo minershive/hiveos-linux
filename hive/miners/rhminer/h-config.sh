242ca77d09bace0edb79c95b70b52a167fc7746c
atrump <trump.andrey@gmail.com>
2019-02-28 00:13:36 +0300

    MINERS NEW rfminer 1.3 - CPU randomhash miner

---
+++ hive/miners/rhminer/h-config.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+function miner_ver() {
+  local MINER_VER=$RHMINER_VER
+  [[ -z $MINER_VER ]] && MINER_VER=$MINER_LATEST_VER
+  echo $MINER_VER
+}
+
+
+function miner_config_echo() {
+  local MINER_VER=`miner_ver`
+  miner_echo_config_file "/hive/miners/$MINER_NAME/$MINER_VER/$MINER_NAME.conf"
+}
+
+function miner_config_gen() {
+  local MINER_CONFIG="$MINER_DIR/$MINER_VER/$MINER_NAME.conf"
+  mkfile_from_symlink $MINER_CONFIG
+
+  local conf=
+
+  [[ -z $RHMINER_TEMPLATE ]] && echo -e "${YELLOW}RHMINER_TEMPLATE is empty${NOCOLOR}" && return 1
+  [[ -z $RHMINER_URL ]] && echo -e "${YELLOW}RHMINER_URL is empty${NOCOLOR}" && return 1
+  local pool1=`head -n 1 <<< "$RHMINER_URL"`
+  local pool2=`head -n 2 <<< "$RHMINER_URL" | tail -n 1`
+  if [[ ! -z $pool2 ]]; then
+    local pass2=$RHMINER_PASS
+    [[ ! -z $pass2 ]] && pass2=" -fop $pass2"
+    pool2=" -fo $pool2 -fou ${RHMINER_TEMPLATE}${pass2}"
+  fi
+
+  local pass=$RHMINER_PASS
+  [[ ! -z $pass ]] && pass=" -pw $pass"
+
+  conf="-s ${pool1} -su ${RHMINER_TEMPLATE}${pass}${pool2} -cpu ${RHMINER_USER_CONFIG} -logfilename $MINER_LOG_BASENAME.log -apiport ${MINER_API_PORT}"
+
+  echo "$conf" > $MINER_CONFIG
+
+}
---
+++ hive/miners/rhminer/h-stats.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+get_cores_hs(){
+  local i=0
+  local l_khs=$1
+  local l_num_cores=$2
+  local l_hs=()
+  for (( i=0; i < ${l_num_cores}; i++ )); do
+    l_hs+=`echo $l_khs | awk '{ printf($1/'$l_num_cores') }'`" "
+  done
+  echo $l_hs
+}
+
+get_cpu_temps(){
+  local i=0
+  local tcore=
+  local l_num_cores=$1
+  local l_temp
+  local coretemp0=`cat /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp*_input 2>/dev/null`
+  [[ ! -z $coretemp0 ]] && #may not work with AMD cpous
+    tcore=$((`cat /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp*_input | head -n 1`/1000)) ||
+    tcore=`cat /sys/class/hwmon/hwmon0/temp*_input | head -n 1 | awk '{print $1/1000}'` #maybe we will need to detect AMD cores
+
+  if [[ ! -z tcore ]]; then
+    for (( i=0; i < ${l_num_cores}; i++ )); do
+      l_temp+="$tcore "
+    done
+    echo $l_temp
+  fi
+}
+
+stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://localhost:$MINER_API_PORT`
+if [[ $? -ne 0 || -z $stats_raw ]]; then
+  echo -e "${YELLOW}Failed to read $miner from localhost:${MINER_API_PORT}${NOCOLOR}"
+else
+  #optiminer sorts it's keys incorrectly, e.g. "GPU1", "GPU10", "GPU2", fixing that with sed hack by replacing "1":{ with "01":{
+  khs=`echo $stats_raw | jq '.speed' | awk '{print $1/1000}'`
+
+  local threads=`echo $stats_raw | jq '.infos[0].threads'`
+  local hs=`get_cores_hs $khs $threads`
+  local temp=`get_cpu_temps $threads`
+  hs=`echo ${hs[@]} | tr " " "\n" | jq -cs '.'`
+  temp=`echo ${temp[@]} | tr " " "\n" | jq -cs '.'`
+
+  stats=$(jq --arg total_khs "$khs" \
+             --argjson hs "$hs" \
+             --arg algo "randomhash" \
+             --arg ver `miner_ver` \
+             --argjson temp "$temp" \
+             '{$total_khs, $hs, $algo, $temp, uptime: .uptime, ar: [.accepted, .rejected, .failed], $ver}' <<< "$stats_raw")
+fi
+
+
+# {
+#   "infos": [
+#     {
+#       "name": "CPU",
+#       "threads": 6,
+#       "speed": 1093,
+#       "accepted": 40,
+#       "rejected": 0,
+#       "temp": 0,
+#       "fan": 0
+#     }
+#   ],
+#   "speed": 1093,
+#   "accepted": 40,
+#   "rejected": 0,
+#   "failed": 0,
+#   "uptime": 2590,
+#   "extrapayload": "",
+#   "stratum.server": "pasc-eu1.nanopool.org:15556",
+#   "stratum.user": "86646-64.daaa095b4dcde5a6.AT_HS",
+#   "diff": 1.526e-05
+# }
