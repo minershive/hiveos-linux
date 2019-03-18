2594b079a14699d9d8efee1d01d68cb42c89ac09
Viktor Komarov <41443179+Kpaccyc@users.noreply.github.com>
2019-03-18 20:55:38 +0300

    Updated stats
    test this

--- hive/miners/xmrig-amd/h-stats.sh
+++ hive/miners/xmrig-amd/h-stats.sh
@@ -9,11 +9,25 @@ else
 	khs=`echo $stats_raw | jq -r '.hashrate.total[0]' | awk '{print $1/1000}'`
 	local temp=$(jq '.temp' <<< $gpu_stats)
 	local fan=$(jq '.fan' <<< $gpu_stats)
-	local ac=$(jq '.results.shares_good' <<< "$stats_raw")
-	local rj=$(( $(jq '.results.shares_total' <<< "$stats_raw") - $ac ))
-	stats=$(jq --argjson temp "$temp" --argjson fan "$fan" --arg ac "$ac" --arg rj "$rj" \
-		'{hs: [.hashrate.threads[][0]], algo: .algo, $temp, $fan, uptime: .connection.uptime,
-		  ar: [$ac, $rj], ver: .version}' <<< "$stats_raw")
+	local hr=`echo $threads_raw | jq -c '.threads[] | {index, hashrate}' | awk -F"," '{a[$1]+=$3}END{for(i in a) print i,a[i]}' | sort | sed -r 's/^[^ ]+//' | tr -d ' '`
+	local gpu_worked=`echo $threads_raw | jq '.threads[].index' | sort | uniq`
+	local gpu_busid=(`echo "$gpu_detect_json" | jq -r '[ . | to_entries[] | select(.value) | .value.busid [0:2] ] | .[]' | sed '/^00/d'`)
+	local busids=''
+	local idx=0
+		for i in $gpu_worked; do
+		gpu=${gpu_busid[$i]}
+		busids[idx]=$((16#$gpu))
+		idx=$((idx+1))
+		done
+	local iv=`cat $MINER_LOG_BASENAME.log | grep -i 'COMPUTE ERROR' | wc -l`
+	stats=$(jq \
+		--arg hs_units "$hs_units" \
+		--argjson temp "$temp" \
+		--argjson fan "$fan" \
+		--arg iv "$iv" \
+		--argjson hr "`echo ${hr} | jq -cs '.'`" \
+		--argjson bus_numbers "`echo ${busids[@]} | jq -cs '.'`" \
+		'{hs: $hr, algo: .algo, $temp, $fan, $hs_units, uptime: .connection.uptime, ar: [.results.shares_good, .results.shares_total - .results.shares_good, $iv], $bus_numbers, ver: .version}' <<< "$stats_raw")
 fi
 
 	[[ -z $khs ]] && khs=0
