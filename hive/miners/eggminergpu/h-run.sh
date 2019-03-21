7d7f59efdf34779ec6c49a1d54c061ad87da6282
atrump <trump.andrey@gmail.com>
2019-03-22 00:16:16 +0300

    MINERS FIX eggminer - free mem on miner restart

--- hive/miners/eggminergpu/h-run.sh
+++ hive/miners/eggminergpu/h-run.sh
@@ -4,18 +4,17 @@
   echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
   exit 1
 
-
+tmp_dir=$MINER_DIR/tmpfs
+$MINER_DIR/h-umount_tmpfs.sh $MINER_DIR/$MINER_VER/eggminer $tmp_dir
 
 if [[ $EGGMINERGPU_TMPFS -eq 1 ]]; then
-  tmp_dir=$MINER_DIR/tmpfs
   [[ ! -d $tmp_dir ]] && mkdir -p $tmp_dir
+
   mount -t tmpfs -o size=1024M tmpfs $tmp_dir
 
   (crontab -l; echo "*/1 * * * * $MINER_DIR/h-umount_tmpfs.sh $MINER_DIR/$MINER_VER/eggminer $tmp_dir") | crontab -
-else
-  tmp_dir=$MINER_DIR/tmp
-  [[ ! -d $tmp_dir ]] && mkdir -p $tmp_dir
 fi
 
 cd $MINER_DIR/$MINER_VER
 ./eggminer 2>&1 | tee $MINER_LOG_BASENAME.log
+
