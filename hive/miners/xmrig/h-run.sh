d2f0e3a319c6cb4fd233d899f1ee28460305dfde
ATrump <trump.andrey@gmail.com>
2019-03-30 16:23:22 +0300

    MINERS MODIFY xmrig - prepare for adding miner forks

--- hive/miners/xmrig/h-run.sh
+++ hive/miners/xmrig/h-run.sh
@@ -4,7 +4,16 @@
 	echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
 	exit 1
 
+#try to release TIME_WAIT sockets
+while true; do
+	for con in `netstat -anp | grep TIME_WAIT | grep $MINER_API_PORT | awk '{print $5}'`; do
+		killcx $con lo
+	done
+	netstat -anp | grep TIME_WAIT | grep $MINER_API_PORT &&
+		continue ||
+		break
+done
 
-cd $MINER_DIR/$MINER_VER
+cd $MINER_DIR/$MINER_FORK/$MINER_VER
 
 ./xmrig | tee $MINER_LOG_BASENAME.log
