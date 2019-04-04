e5ddc5caf7fa3972e7dca3bcd4e776bf36562ca7
ATrump <trump.andrey@gmail.com>
2019-04-05 00:36:15 +0300

    MINERS FIX noncepool-amd - add bus_numbers

--- hive/miners/noncepool-amd/h-stats.sh
+++ hive/miners/noncepool-amd/h-stats.sh
@@ -6,6 +6,8 @@ if [[ $? -ne 0  || -z $threads ]]; then
 else
   summary=`echo "summary" | nc -w 2 localhost ${MINER_API_PORT} | tr -d '\0'`
   re=';UPTIME=([0-9]+);' && [[ $summary =~ $re ]] && local uptime=${BASH_REMATCH[1]} #&& echo "Matched" || echo "No match"
+  #saving log head to read bus_numbers
+  [[ $uptime -lt 60 ]] && head -n 50 ${MINER_LOG_BASENAME}.log > ${MINER_LOG_BASENAME}_head.log
   #khs will calculate from cards; re=';KHS=([0-9\.]+);' && [[ $summary =~ $re ]] && khs=${BASH_REMATCH[1]} #&& echo "Matched" || echo "No match"
   #algo=`echo "$summary" | tr ';' '\n' | grep -m1 'ALGO=' | sed -e 's/.*=//'`
   local algo="sha224"
@@ -18,28 +20,34 @@ else
   #if GPU has 0.0 temp it hanged. ccminer does not mine on this card but shows hashrate
   #cctemps=(`echo "$striplines" | grep 'TEMP=' | sed -e 's/.*=//'`) #echo ${cctemps[@]} | tr " " "\n" #print it in lines
   cckhs=(`echo "$striplines" | grep 'KHS=' | sed -e 's/.*=//'`)
-  ccbusids=(`echo "$striplines" | grep 'BUS=' | sed -e 's/.*=//'`)
-  local bus_numbers=$(jq -sc . <<< "${ccbusids[*]}")
 
+  local bus_ids=""
+  local a_fans=""
+  local a_temp=""
 
-  #local nvidiastats
   for (( i=0; i < ${#cckhs[@]}; i++ )); do
-    #if temp is 0 then driver or GPU failed
-    [[ ${cctemps[$i]} == "0.0" ]] && cckhs[$i]="0.0"
 
     #cckhs[$i]="84316579.94" #test
     #check Ghs. 1080ti gives ~64mh (64000kh) on lyra. when it's showing ghs then load is 0 on gpu
     #if [[ `echo ${cckhs[$i]} | awk '{ print ($1 >= 1000000) ? 1 : 0 }'` == 1 ]]; then #hash is in Ghs, >= 1000000 khs
     if [[ `echo ${cckhs[$i]} | awk '{ print ($1 >= 1000) ? 1 : 0 }'` == 1 ]]; then # > 1Mh
       #[[ -z $nvidiastats ]] && nvidiastats=`gpu-stats nvidia` #a bit overhead in calling nvidia-smi again
-      local busid=`echo ${ccbusids[$i]} | awk '{ printf("%02x:00.0", $1) }'` #ccbus is decimal
-      local load_i=`echo "$gpu_stats" | jq ".busids|index(\"$busid\")"`
-      if [[ $load_i != "null" ]]; then #can be null on failed driver
-        local load=`echo "$gpu_stats" | jq -r ".load[$load_i]"`
-        #load=0 #test
-        [[ -z $load || $load -le 10 ]] &&
-          echo -e "${RED}Hash on GPU$i is in GH/s (${cckhs[$i]} kH/s) but Load is detected to be only $load%${NOCOLOR}" &&
-          cckhs[$i]="0.0"
+      local busid=`cat ${MINER_LOG_BASENAME}_head.log | grep -A1 "Card $i is at pci slot name"| tail -1 | cut -d " " -f 3` #ccbus is decimal
+      bus_ids+=$busid" "
+
+      #convert busid to hex
+      busid=`echo $busid | awk '{ printf("%02x:00.0", $1) }'`
+      local gpu_i=`echo "$gpu_stats" | jq ".busids|index(\"$busid\")"`
+      if [[ $gpu_i != "null" ]]; then #can be null on failed driver
+        local t_fan=`echo "$gpu_stats" | jq -r ".fan[$gpu_i]"`
+        a_fans+=$t_fan" "
+
+        local t_temp=`echo "$gpu_stats" | jq -r ".temp[$gpu_i]"`
+        a_temp+=$t_temp" "
+      else
+        a_fans+="0 "
+        a_temp+="0 "
+        cckhs[$i]="0.0"
       fi
     fi
 
@@ -47,8 +55,6 @@ else
     khs=`echo $khs ${cckhs[$i]} | awk '{ printf("%.3f", $1 + $2) }'`
   done
 
-  local temp=$(jq '.temp' <<< $gpu_stats)
-  local fan=$(jq '.fan' <<< $gpu_stats)
   [[ $cpu_indexes_array != '[]' ]] && #remove Internal Gpus
     temp=$(jq -c "del(.$cpu_indexes_array)" <<< $temp) &&
     fan=$(jq -c "del(.$cpu_indexes_array)" <<< $fan)
@@ -59,8 +65,9 @@ else
     --arg algo "$algo" \
     --argjson hs "`echo ${cckhs[@]} | tr " " "\n" | jq -cs '.'`" \
     --arg hs_units "khs" \
-    --argjson temp "$temp" \
-    --argjson fan "$fan" \
+    --argjson fan "`echo ${a_fans[@]} | tr " " "\n" | jq -cs '.'`" \
+    --argjson temp "`echo ${a_temp[@]} | tr " " "\n" | jq -cs '.'`" \
+    --argjson bus_numbers "`echo ${bus_ids[@]} | tr " " "\n" | jq -cs '.'`" \
     --arg ver "$ver" \
-    '{$hs, $hs_units, $temp, $fan, uptime:'$uptime', ar: ['$ac', '$rj'], $algo, $ver}')
+    '{$hs, $hs_units, $temp, $fan, $bus_numbers, uptime:'$uptime', ar: ['$ac', '$rj'], $algo, $ver}')
 fi
