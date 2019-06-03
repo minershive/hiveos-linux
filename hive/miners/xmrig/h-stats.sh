282732f8b9fda27fe637533d48d530ed0ce19fba
HaloGenius <halogenius@yandex.ru>
2019-06-03 17:00:40 +0300

    MINERS FIX XMRig - fix CPU temp on some MB

--- hive/miners/xmrig/h-stats.sh
+++ hive/miners/xmrig/h-stats.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+function get_cpu_temp(){
+    local HWMON_DIR=`ls -l /sys/class/hwmon | grep coretemp | awk '{ print $9 }'`
+    if [[ -z $HWMON_DIR ]]; then
+	HWMON_DIR="/sys/class/hwmon/hwmon0"
+    else
+	HWMON_DIR="/sys/class/hwmon/$HWMON_DIR"
+    fi
+    cat $HWMON_DIR/temp*_input
+}
+
 stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:$MINER_API_PORT`
 #echo $stats_raw | jq .
 if [[ $? -ne 0 || -z $stats_raw ]]; then
@@ -8,7 +18,7 @@ else
 	khs=`echo $stats_raw | jq -r '.hashrate.total[0]' | awk '{print $1/1000}'`
 	local ac=$(jq '.results.shares_good' <<< "$stats_raw")
 	local rj=$(( $(jq '.results.shares_total' <<< "$stats_raw") - $ac ))
-	local cpu_temp=`cat /sys/class/hwmon/hwmon0/temp*_input | head -n $(nproc) | awk '{print $1/1000}' | jq -rsc .` #just a try to get CPU temps
+	local cpu_temp=`get_cpu_temp | head -n $(nproc) | awk '{print $1/1000}' | jq -rsc .` #just a try to get CPU temps
 	stats=`echo $stats_raw | jq --arg ac "$ac" --arg rj "$rj" \
 					'{hs: [.hashrate.threads[][0]], temp: '$cpu_temp', ar: [$ac, $rj],
 					  uptime: .connection.uptime, algo: .algo, ver: .version}'`
