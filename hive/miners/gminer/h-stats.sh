6b6f52c207e26254e832c3a73fa2e65a38c5b875
ATrump <trump.andrey@gmail.com>
2019-08-19 13:56:59 +0300

    MINERS FIX gminer - fix temp in stats

--- hive/miners/gminer/h-stats.sh
+++ hive/miners/gminer/h-stats.sh
@@ -15,6 +15,8 @@ else
 
   #All fans speed array
   local fan=$(jq -r ".fan | .[]" <<< $gpu_stats)
+  #All temp array
+  local temp=$(jq -r ".temp | .[]" <<< $gpu_stats)
   #gminer's busid array
   local bus_id_array=$(jq -r '[.devices[].bus_id[5:7]] | .[]' <<< "$stats_raw")
   local bus_numbers=$(echo "$bus_id_array" | awk '{printf("%d\n", "0x"$1)}' | jq -sc .)
@@ -25,14 +27,18 @@ else
 #  fan=`sed 's/\n/ /' <<< $fan`
   bus_id_array=`tr '\n' ' ' <<< $bus_id_array`
   fan=`tr '\n' ' ' <<< $fan`
+  temp=`tr '\n' ' ' <<< $temp`
   IFS=' ' read -r -a bus_id_array <<< "$bus_id_array"
   IFS=' ' read -r -a fan <<< "$fan"
+  IFS=' ' read -r -a temp <<< "$temp"
   #busid equality
   local fans_array=
+  local temp_array=
   for ((i = 0; i < ${#all_bus_ids_array[@]}; i++)); do
     for ((j = 0; j < ${#bus_id_array[@]}; j++)); do
       if [[ "$(( 0x${all_bus_ids_array[$i]} ))" -eq "$(( 0x${bus_id_array[$j]} ))" ]]; then
         fans_array+=("${fan[$i]}")
+        temp_array+=("${temp[$i]}")
       fi
     done
   done
@@ -42,9 +48,11 @@ else
   [[ "$GMINER_ALGO" == "beamhash" ]] && GMINER_ALGO="equihash 150/5"
   [[ "$GMINER_ALGO" == "beamhashII" ]] && GMINER_ALGO="equihash 150/5/3"
 
-  local temp=$(jq -c "[.temp$nvidia_indexes_array]" <<< $gpu_stats)
+  #local temp=$(jq -c "[.temp$nvidia_indexes_array]" <<< $gpu_stats)
 
-  stats=$(jq -c --argjson temp "$temp" --argjson fan "`echo "${fans_array[@]}" | jq -s . | jq -c .`" \
+  stats=$(jq -c \
+        --argjson temp "`echo "${temp_array[@]}" | jq -s . | jq -c .`" \
+        --argjson fan "`echo "${fans_array[@]}" | jq -s . | jq -c .`" \
         --arg ac "$ac" --arg rj "$rj" \
         --argjson bus_numbers "$bus_numbers" --arg algo "$GMINER_ALGO"  \
         --arg ver `miner_ver` \
