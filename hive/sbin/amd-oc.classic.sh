6c3fb2434c98a9370ef1887dd7e3e71f54af84a8
Игорь <igor.villos@gmail.com>
2019-09-15 10:54:41 +0300

    small fix fan control

--- hive/sbin/amd-oc.classic.sh
+++ hive/sbin/amd-oc.classic.sh
@@ -74,9 +74,9 @@ else
 	echo $oc_cmd
 	eval $oc_cmd
 
-	#echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
+	echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
 	
-	#[[ ! -z $FAN && ${FAN[$i]} > 0 ]] && wolfamdctrl -i $cardno --set-fanspeed ${FAN[$i]}
+	[[ ! -z $FAN && ${FAN[$i]} > 0 ]] && wolfamdctrl -i $cardno --set-fanspeed ${FAN[$i]}
 	[[ ! -z $REF && ${REF[$i]} > 0 ]] && amdmemtweak --gpu $i --REF ${REF[$i]}
 	
 fi
