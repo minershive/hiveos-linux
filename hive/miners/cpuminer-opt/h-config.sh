9c177b50df79dd1bdd2b77331f11d8cc2f1aa219
ATrump <trump.andrey@gmail.com>
2019-09-27 17:12:52 +0300

    MINERS MODIFY - CPUminer-opt-JayDDee v3.9.8

--- hive/miners/cpuminer-opt/h-config.sh
+++ hive/miners/cpuminer-opt/h-config.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
+function miner_fork() {
+	local MINER_FORK=$CPUMINER_OPT_FORK
+	[[ -z $MINER_FORK ]] && MINER_FORK=$MINER_DEFAULT_FORK
+	echo $MINER_FORK
+}
+
 function miner_ver() {
 	local MINER_VER=$CPUMINER_OPT_VER
-	[[ -z $MINER_VER ]] && MINER_VER=$MINER_LATEST_VER
+	local fork=${MINER_FORK^^} #uppercase MINER_FORK
+	[[ -z $MINER_VER ]] && eval "MINER_VER=\$MINER_LATEST_VER_${fork//-/_}" #char replace
 	echo $MINER_VER
 }
 
-
 function miner_config_echo() {
+	local MINER_FORK=`miner_fork`
 	local MINER_VER=`miner_ver`
-	miner_echo_config_file "$MINER_DIR/$MINER_VER/cpuminer.conf"
+	miner_echo_config_file "$MINER_DIR/$MINER_FORK/$MINER_VER/cpuminer.conf"
 }
 
 
 function miner_config_gen() {
-	local MINER_CONFIG="$MINER_DIR/$MINER_VER/cpuminer.conf"
+	local MINER_CONFIG="$MINER_DIR/$MINER_FORK/$MINER_VER/cpuminer.conf"
 	mkfile_from_symlink $MINER_CONFIG
 
-	conf=`cat $MINER_DIR/$MINER_VER/config_global.json | envsubst`
+	conf=`cat $MINER_DIR/$MINER_FORK/$MINER_VER/config_global.json | envsubst`
 
 	[[ ! -z $CPUMINER_OPT_TEMPLATE ]] &&
 		conf=`jq --null-input --argjson conf "$conf" --arg user "$CPUMINER_OPT_TEMPLATE" '$conf + {$user}'`
--- hive/miners/cpuminer-opt/h-run.sh
+++ hive/miners/cpuminer-opt/h-run.sh
@@ -38,8 +38,8 @@ else
 	CPU_FLAG="sse2"
 fi
 
-echo -e "Running ${CYAN}cpuminer-opt${NOCOLOR} optimized for ${GREEN}$CPU_FLAG${NOCOLOR}" | tee ${MINER_LOG_BASENAME}.log
+echo -e "Running ${CYAN}cpuminer-opt-$MINER_FORK${NOCOLOR} optimized for ${GREEN}$CPU_FLAG${NOCOLOR}" | tee ${MINER_LOG_BASENAME}.log
 
-cd ${MINER_DIR}/${MINER_VER}
+cd ${MINER_DIR}/${MINER_FORK}/${MINER_VER}
 
 ./cpuminer-$CPU_FLAG -c cpuminer.conf 2>&1 | tee ${MINER_LOG_BASENAME}.log
