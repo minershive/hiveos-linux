754d1d3e491770388cd2fde617b9bc3641e8e20c
biospb <42325079+biospb@users.noreply.github.com>
2019-10-03 18:12:07 +0300

    LINUX FIX opendev-watchdog minor fixes
    blacklisted hidraw0 for some buggy watchdogs,  improved restart

--- hive/opt/opendev/watchdog-opendev.sh
+++ hive/opt/opendev/watchdog-opendev.sh
@@ -6,11 +6,11 @@
 
 
 #grep processes but not a self sh script
-PREVPID=`ps aux | grep "watchdog-opendev" | grep -v 'watchdog-opendev.sh' | grep -v grep | awk '{print $2}'`
-if [[ ! -z $PREVPID ]]; then
-	echo "Killing Watchdog OpenDev existing process $PREVPID"
-	kill -9 $PREVPID
-fi
+PREVPID=(`ps aux | grep "watchdog-opendev" | grep -v 'watchdog-opendev.sh' | grep -v grep | awk '{print $2}'`)
+for pid in "${PREVPID[@]}"; do
+	echo "Killing Watchdog OpenDev existing process $pid"
+	sudo kill -9 $pid
+done
 
 
 c=`lsusb | grep -E '0483:5740|0483:a26d' | wc -l`
@@ -33,10 +33,9 @@ for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
 	[[ $ID_VENDOR_ID != "0483" ]] && continue
 	[[ ! ($ID_MODEL_ID == "5740" || $ID_MODEL_ID == "a26d") ]] && continue
 	#echo "/dev/$devname - $ID_SERIAL"
-	echo "$DEVNAME - $ID_SERIAL"
-
 	#Starting actual daemon for watchdog
 	#path=$(dirname $(realpath $0))
 	nohup /hive/opt/opendev/watchdog-opendev ping $DEVNAME > /dev/null 2>&1 &
+	echo "$DEVNAME - $ID_SERIAL ($!)"
 done
 
