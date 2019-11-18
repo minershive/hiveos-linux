15519397235342307cf5b5a3f829a971c772e30a
ATrump <trump.andrey@gmail.com>
2019-11-18 20:09:14 +0300

    MINERS FIX nq-miner - fix stats

--- hive/miners/nq-miner/h-stats.sh
+++ hive/miners/nq-miner/h-stats.sh
@@ -4,14 +4,23 @@ if [[ $? -ne 0  || -z $stats_raw ]]; then
   echo -e "${YELLOW}Failed to read $miner stats from localhost:${MINER_API_PORT}${NOCOLOR}"
 else
   algo="argon2d-nim"
+
+  local temp=$(jq '.temp' <<< $gpu_stats)
+  local fan=$(jq '.fan' <<< $gpu_stats)
+
+  [[ $cpu_indexes_array != '[]' ]] && #remove Internal Gpus
+    temp=$(jq -c "del(.$cpu_indexes_array)" <<< $temp) &&
+    fan=$(jq -c "del(.$cpu_indexes_array)" <<< $fan)
+
   ver=`echo $stats_raw | jq -r '.version' | cut -d " " -f 3-`
 
   stats=$(jq -c --arg algo "$algo" \
                 --arg hs_units "hs" \
                 --arg ver "$ver" \
                 --argjson gpu_stats "$gpu_stats" \
-                '{ total_khs: (.totalHashrate | floor / 1000), hs: (.hashrates | map(. | floor)), hs_units: "hs",
-                temp: $gpu_stats.temp, fan: $gpu_stats.fan, uptime, $ver, ar: [.shares - .errors, .errors], $algo}' <<< "$stats_raw")
+                --argjson fan "$fan" --argjson temp "$temp" \
+                '{ total_khs: (.totalHashrate | floor / 1000), hs: ([.hashrates[] | select (. != null ) | floor]), hs_units: "hs",
+                $temp, $fan, uptime, $ver, ar: [.shares - .errors, .errors], $algo}' <<< "$stats_raw")
   khs=$(jq -r '.total_khs' <<< "$stats")
 fi
 
