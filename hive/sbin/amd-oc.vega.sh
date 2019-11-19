861430201fc3afcf7c6daf8f12145b7356dc512f
villos <igor.villos@gmail.com>
2019-11-19 21:38:13 +0200

    ADD: Implement Navi10 OC|UV  support

--- hive/sbin/amd-oc.vega.sh
+++ hive/sbin/amd-oc.vega.sh
@@ -2,9 +2,13 @@
 
 
 VEGA20=$( lspci -vnns $busid | grep VGA -A 2 | grep AMD -A 2 | grep Vega -A 2 | grep "Vega 20" | wc -l )
+#NAVI=$( lspci -vnns $busid | grep Navi | wc -l )
+
+echo "manual" > /sys/class/drm/card$cardno/device/power_dpm_force_performance_level
+
 
 function _SetcoreVDDC {
-	if [[ $VEGA20 -ne 0 ]]; then
+	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0  ]]; then
 		echo "Noop"
 	else
 	vegatool -i $cardno --volt-state 7 --vddc-table-set $1 --vddc-mem-table-set $1
@@ -13,9 +17,11 @@ function _SetcoreVDDC {
 
 function _SetcoreClock {
 	local vdd=$2
-	if [[ $VEGA20 -ne 0 ]]; then
+	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
 		echo "s 1 $1" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
-		[[  -z $vdd  ]] && vdd="1050" 
+		[[  -z $vdd  ]] && vdd="1050"
+		[[  -z $vdd  && $NAVI_COUNT -ne 0 ]] && vdd="0"
+		 
 		echo "vc 2 $1 $vdd" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
 		echo c > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
 
@@ -28,7 +34,7 @@ function _SetcoreClock {
 }	
 
 function _SetmemClock {
-	if [[ $VEGA20 -ne 0 ]]; then
+	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
 		echo "m 1 $1" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
 		echo c > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
 	else
@@ -52,8 +58,21 @@ fi
 
 	echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
 	echo "manual" > /sys/class/drm/card$cardno/device/power_dpm_force_performance_level
-	echo 4 > /sys/class/drm/card$cardno/device/pp_power_profile_mode
-	vegatool -i $cardno --set-fanspeed 50
+	echo 5 > /sys/class/drm/card$cardno/device/pp_power_profile_mode
+	#vegatool -i $cardno --set-fanspeed 50
+	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
+		rocm-smi -d  $cardno --setfan 50%
+	else	
+		vegatool -i $cardno  --set-fanspeed 50}
+	fi
+
+
+
 
 [[ ! -z $FAN && ${FAN[$i]} > 0 ]] &&
-	vegatool -i $cardno  --set-fanspeed ${FAN[$i]}
+	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
+		rocm-smi -d  $cardno --setfan ${FAN[$i]}%
+	else	
+		vegatool -i $cardno  --set-fanspeed ${FAN[$i]}
+	fi
+	
