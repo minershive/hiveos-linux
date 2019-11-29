fc3618c513b968e4633df1b782f4f2cdbe01e0aa
ATrump <trump.andrey@gmail.com>
2019-11-29 21:41:35 +0300

    MINERS ADD finminer - add null bus ids for CPU threads

--- hive/miners/finminer/h-stats.sh
+++ hive/miners/finminer/h-stats.sh
@@ -24,6 +24,25 @@ get_cpu_temps(){
 	fi
 }
 
+get_bus_numbers () {
+	#2019-11-29 20:58:18: <info> GPU 0 PCI 01.00.0, Platform: CUDA, Name: GeForce GTX 1050 Ti, 4040 MB available
+  local line=
+	local l_bus_numbers=
+	while read -r line ; do
+		l_bus_numbers+=`echo $line | cut -d " " -f 7 | cut -d "." -f 1 | awk '{printf("%d\n", "0x"$1)}'`" "
+	done < <(cat ${MINER_LOG_BASENAME}_head.log | grep "<info> GPU" | grep "PCI")
+	echo $l_bus_numbers
+}
+
+get_cpu_bus_numbers () {
+	local i=0
+	local l_bus=
+	for (( i=0; i < $1; i++ )); do
+		l_bus+="null "
+	done
+	echo $l_bus
+}
+
 stats_raw=`echo '{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}' | nc -w $API_TIMEOUT localhost $MINER_API_PORT | jq '.result'`
 if [[ $? -ne 0  || -z $stats_raw ]]; then
 	echo -e "${YELLOW}Failed to read $miner stats_raw from localhost:${MINER_API_PORT}${NOCOLOR}"
@@ -43,10 +62,12 @@ else
 	local hs_units=''
 	[[ $algo == "ethash" ]] && hs_units="khs" || hs_units="hs"
 
+	[[ $uptime -lt 60 ]] && head -n 50 $MINER_LOG_BASENAME.log > ${MINER_LOG_BASENAME}_head.log
+
 	if [[ $algo == "randomhash" ]]; then
-		[[ $uptime -lt 60 ]] && head -n 50 $MINER_LOG_BASENAME.log > ${MINER_LOG_BASENAME}_head.log
 		#2018-12-20 22:22:44: <info> Using CPU threads: 6
 		num_cores=`cat ${MINER_LOG_BASENAME}_head.log | grep "<info> Using CPU threads:" | awk '{print $7}'`
+		bus_numbers=`get_cpu_bus_numbers $num_cores | tr " " "\n" | jq -cs '.'`
 		khs=`get_total_hs`
 		get_cores_hs
 		hs=`echo ${hs[@]} | tr " " "\n" | jq -cs '.'`
@@ -55,6 +76,7 @@ else
 		temp=`echo ${temp[@]} | tr " " "\n" | jq -cs '.'`
 		fan="[]"
 	else
+		bus_numbers=`get_bus_numbers | tr " " "\n" | jq -cs '.'`
 		khs=`echo $stats_raw | jq -r '.[2]' | awk -F';' '{print $1}'`
 		local tempfans=`echo $stats_raw | jq -r '.[6]' | tr ';' ' '`
 
@@ -75,7 +97,8 @@ else
 		--argjson hs "$hs" --argjson temp "$temp" --argjson fan "$fan" \
 		--arg ac "$ac" --arg rj "$rj" \
 		--arg algo "$algo" --arg hs_units "$hs_units" --arg ver "$ver" \
-		'{$hs, $hs_units, $temp, $fan, $uptime, $algo, ar: [$ac, $rj], $ver}')
+		--argjson bus_numbers "$bus_numbers" \
+		'{$hs, $hs_units, $temp, $fan, $uptime, $algo, ar: [$ac, $rj], $bus_numbers, $ver}')
 fi
 
 [[ -z $khs ]] && khs=0
