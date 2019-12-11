9fc3d6932882019a42d4a52a08a2bb4f634943f0
HaloGenius <halogenius@yandex.ru>
2019-12-11 21:01:59 +0200

    MINERS FIX XMRig (unified) turn off autotune by `hugepages` for version higher then v5.2.0

--- hive/miners/xmrig-new/h-run.sh
+++ hive/miners/xmrig-new/h-run.sh
@@ -16,7 +16,7 @@ function HugePagesTune(){
 	echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
 	exit 1
 
-[[ "$XMRIG_NEW_VER" == "" || "$XMRIG_NEW_VER" < "5.2.0" ]] && HugePagesTune
+[[ "$XMRIG_NEW_VER" != "" || "$XMRIG_NEW_VER" < "5.2.0" ]] && HugePagesTune
 
 # Miner run here
 cd $MINER_DIR/$MINER_FORK/$MINER_VER
