d28121e402e1e0ff6bc54ebb8029427029b13a38
HaloGenius <halogenius@yandex.ru>
2019-12-11 13:43:21 +0200

    MINERS UPDATE XMRig (unified) v5.2.0 * added 1GB huge pages support (new option `1gb-pages` in `randomx` object) * added automatic huge pages configuration * added automatic Intel prefetchers configuration (new option `wrmsr` in `randomx` object) * new performance optimizations for Ryzen CPUs * added `max-threads-hint` option support for RandomX dataset initialization threads * added `priority` option support for RandomX dataset initialization threads

--- hive/miners/xmrig-new/h-run.sh
+++ hive/miners/xmrig-new/h-run.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+# HugePages tunning
+function HugePagesTune(){
+   echo "Start HugePages tuning for RandomX ..."
+   if [[ ! -z $XMRIG_NEW_HUGEPAGES ]]; then
+       hugepages $XMRIG_NEW_HUGEPAGES
+   else
+      if [[ `echo $XMRIG_NEW_ALGO | grep -c "^rx\/"` -gt 0 ]]; then
+          hugepages -rx $XMRIG_NEW_HUGEPAGES
+      fi
+   fi
+}
+
 [[ `ps aux | egrep './[x]mrig' | grep -v xmrig-amd | grep -v xmrig-nvidia | wc -l` != 0 ]] &&
 	echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
 	exit 1
 
-# HugePages tunning
-if [[ ! -z $XMRIG_NEW_HUGEPAGES ]];
-then
-    hugepages $XMRIG_NEW_HUGEPAGES
-else
-   if [[ `echo $XMRIG_NEW_ALGO | grep -c "^rx\/"` -gt 0 ]];
-   then
-       hugepages -rx $XMRIG_NEW_HUGEPAGES
-   fi
-fi
+[[ "$XMRIG_NEW_VER" == "" || "$XMRIG_NEW_VER" < "5.2.0" ]] && HugePagesTune
 
 # Miner run here
 cd $MINER_DIR/$MINER_FORK/$MINER_VER
