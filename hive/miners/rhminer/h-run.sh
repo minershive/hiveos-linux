9fe93637cb21f27d25e66bf3abc5c8e6874aeb7f
ATrump <trump.andrey@gmail.com>
2020-01-18 06:50:16 +0300

    MINERS FIX rhminer * fix signal bug

--- hive/miners/rhminer/h-run.sh
+++ hive/miners/rhminer/h-run.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 [[ `ps aux | grep "./rhminer" | grep -v grep | wc -l` != 0 ]] &&
-	echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
-	exit 1
+  echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
+  exit 1
 
-#try to release TIME_WAIT sockets
-while true; do
-	for con in `netstat -anp | grep TIME_WAIT | grep $MINER_API_PORT | awk '{print $5}'`; do
-		killcx $con lo
-	done
-	netstat -anp | grep TIME_WAIT | grep $MINER_API_PORT &&
-		continue ||
-		break
-done
+##try to release TIME_WAIT sockets
+#while true; do
+#  for con in `netstat -anp | grep TIME_WAIT | grep $MINER_API_PORT | awk '{print $5}'`; do
+#    killcx $con lo
+#  done
+#  netstat -anp | grep TIME_WAIT | grep $MINER_API_PORT &&
+#    continue ||
+#    break
+#done
 
 cd $MINER_DIR/$MINER_VER
 
-./rhminer `cat rhminer.conf`
+unbuffer ./rhminer `cat rhminer.conf`
