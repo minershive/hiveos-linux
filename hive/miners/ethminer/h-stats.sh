75f9d1b1879a5146714c6e09c8fc856a37e0523d
ATrump <trump.andrey@gmail.com>
2020-02-17 23:27:17 +0300

    MINERS ADD ethminer - add bus numbers, invalid shares, invalid per GPU for version 0.18.0+

--- hive/miners/ethminer/h-stats.sh
+++ hive/miners/ethminer/h-stats.sh
@@ -36,12 +36,40 @@ else
 	[[ $ETHMINER_FORK == "ubqminer" ]] && algo="ubiqhash"
 	[[ $ETHMINER_FORK == "zilminer" ]] && algo="zilliqahash"
 	[[ $ETHMINER_FORK == "teominer" ]] && algo="tethashv1"
-	stats=$(jq -n \
-		--arg uptime "`echo \"$stats_raw\" | jq -r '.[1]' | awk '{print $1*60}'`" \
-		--argjson hs "$hs" --argjson temp "$temp" --argjson fan "$fan" \
-		--arg algo "$algo" \
-		--arg ac "$ac" --arg rj "$rj" \
-		--arg ver "$ver" \
-		'{$hs, $temp, $fan, $uptime, $algo, ar: [$ac, $rj], $ver}')
-		#TODO: bus_numbers
+
+	local stats_detail=`echo '{"id":0,"jsonrpc":"2.0","method":"miner_getstatdetail"}' | nc -w $API_TIMEOUT localhost $MINER_API_PORT | jq '.result'`
+	if [[ $? -ne 0 || -z $stats_detail || $stats_detail == "null" ]]; then
+		local bus_numbers=[]
+		local iv=0
+	else
+		if [[ ! -z `echo $stats_detail | jq '. | to_entries[] | select(.key == "devices")'` ]]; then #version >= 0.18.0
+			local bus_numbers=`echo $stats_detail | jq -r '.devices[].hardware.pci [0:2]' | awk -Wposix '{printf("%d\n","0x" $1)}' | jq -cs '.'`
+			local iv=`echo $stats_detail | jq -r '.mining.shares[2]'`
+			local iv_bus=`echo $stats_detail | jq '.devices[].mining.shares[2]' | jq -cs '.' | sed  's/,/;/g' | tr -d [ | tr -d ]`
+		elif [[ ! -z `echo $stats_detail | jq '. | to_entries[] | select(.key == "gpus")'` ]]; then #version < 0.18.0
+			local bus_numbers=[]
+			local iv=`echo $stats_detail | jq '.shares.invalid'`
+		else #version unknown
+			local bus_numbers=[]
+			local iv=0
+		fi
+	fi
+	if [[ -z $bus_numbers || $bus_numbers == "[]" ]]; then
+		stats=$(jq -n \
+			--arg uptime "`echo \"$stats_raw\" | jq -r '.[1]' | awk '{print $1*60}'`" \
+			--argjson hs "$hs" --argjson temp "$temp" --argjson fan "$fan" \
+			--arg algo "$algo" \
+			--arg ac "$ac" --arg rj "$rj" --arg iv "$iv" \
+			--arg ver "$ver" \
+			'{$hs, $temp, $fan, $uptime, $algo, ar: [$ac, $rj, $iv], $ver}')
+	else
+		stats=$(jq -n \
+			--arg uptime "`echo \"$stats_raw\" | jq -r '.[1]' | awk '{print $1*60}'`" \
+			--argjson hs "$hs" --argjson temp "$temp" --argjson fan "$fan" \
+			--arg algo "$algo" \
+			--arg ac "$ac" --arg rj "$rj" --arg iv "$iv" --arg iv_bus "$iv_bus" \
+			--arg ver "$ver" \
+			--argjson bus_numbers "$bus_numbers" \
+			'{$hs, $temp, $fan, $uptime, $algo, ar: [$ac, $rj, $iv, $iv_bus], $bus_numbers, $ver}')
+	fi
 fi
