adfed70e0cd28a46773469fcd98d99572b2edec1
biospb <42325079+biospb@users.noreply.github.com>
2020-02-22 18:54:16 +0300

    LINUX ADD Polaris overclocking improvements
    greatly improved speed and stability especially in aggressive mode
    reduced idle power usage if dpm/mdpm was used
    fixed memory clock settings in mdpm 1 mode
    check and set bios max clock values if needed
    report bios TDP values in oc log

--- hive/sbin/amd-oc.aggressive.sh
+++ hive/sbin/amd-oc.aggressive.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
-#Restore saved Power Play table
-cp $savedpp /sys/class/drm/card$cardno/device/pp_table
-sync
+# applying all at once does not always work with running miner due to driver issues
+FAST=0
 
-#coreState=`wolfamdctrl -i $cardno --show-core | grep -E "DPM state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/'`
+PPT=/tmp/pp_table$cardno
+CARDPPT=/sys/class/drm/card$cardno/device/pp_table
+
+# using saved Power Play table
+cp $savedpp $PPT
+
+#coreState=`ohgodatool -p $PPT --show-core | grep -E "DPM state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/'`
 args=''
 coreState=
 
-#memoryState=
-#memoryState=1 #hello ethos
-#getting max mem state
-memoryState=`wolfamdctrl -i $cardno --show-mem | grep -E "Memory state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/'`
+# getting max mem state
+#maxMemoryState=`ohgodatool -p $PPT --show-mem | grep -E "Memory state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/'`
+maxMemoryState=`ohgodatool -p $PPT --show-mem | grep -oP "Memory state \K[0-9]+" | tail -n 1`
+maxMemoryClock=`ohgodatool -p $PPT --show-max-mem-clock | grep -oP "\K[0-9]+"`
+maxCoreClock=`ohgodatool -p $PPT --show-max-core-clock | grep -oP "\K[0-9]+"`
+maxPower=`ohgodatool -p $PPT --show-max-power | grep -oP "\K[0-9]+"`
+TDP=`ohgodatool -p $PPT --show-tdp | grep -oP "\K[0-9]+"`
+TDC=`ohgodatool -p $PPT --show-tdc | grep -oP "\K[0-9]+"`
+#echo "Max core: ${maxCoreClock}MHz, Max mem: ${maxMemoryClock}MHz"
+echo "TDP: ${TDP}W, TDC: ${TDC}A, Max power: ${maxPower}W"
 
 
 if [[ ! -z $CORE_STATE ]]; then
@@ -25,64 +36,58 @@ if [[ ! -z $CORE_STATE ]]; then
 fi
 
 
-if [[ ! -z $MEM_STATE ]]; then
-	if [[ ${MEM_STATE[$i]} -ge 0 && ${MEM_STATE[$i]} -le $memoryState ]]; then
-		[[ ${MEM_STATE[$i]} != 0 ]] && #skip zero state, means auto
-			memoryState=${MEM_STATE[$i]}
-	else
-		echo -e "${YELLOW}WARNING: Invalid mem state ${MEM_STATE[$i]}, falling back to $memoryState${NOCOLOR}"
-	fi
+if [[ -z ${MEM_STATE[$i]} || ${MEM_STATE[$i]} -le 0 || ${MEM_STATE[$i]} -gt $maxMemoryState ]]; then
+	[[ ! -z ${MEM_STATE[$i]} && ${MEM_STATE[$i]} -ne 0 ]] && echo -e "${YELLOW}WARNING: Invalid mem state ${MEM_STATE[$i]}, falling back to $maxMemoryState${NOCOLOR}"
+	${MEM_STATE[$i]}=$maxMemoryState
 fi
 
 
-if [[ ! -z $MEM_CLOCK && ${MEM_CLOCK[$i]} > 0 ]]; then
-	#echo "Setting mem to ${MEM_CLOCK[$i]}" &&
-	wolfamdctrl -i $cardno --mem-clock ${MEM_CLOCK[$i]} --mem-state $memoryState
-#		args+=" --mem-clock ${MEM_CLOCK[$i]} --mem-state $memoryState"
+if [[ ! -z $MEM_CLOCK && ${MEM_CLOCK[$i]} -gt 0 ]]; then
+	ohgodatool -p $PPT --mem-clock ${MEM_CLOCK[$i]} --mem-state ${MEM_STATE[$i]}
+	# fix mem clock setting with MDPM=1 on some gpu/driver/kernel combinations
+	[[ ${MEM_STATE[$i]} -ne $maxMemoryState ]] &&
+		ohgodatool -p $PPT --mem-clock ${MEM_CLOCK[$i]} --mem-state $maxMemoryState
+	[[ ${MEM_CLOCK[$i]} -gt $maxMemoryClock ]] &&
+		ohgodatool -p $PPT --set-max-mem-clock ${MEM_CLOCK[$i]}
+	[[ $FAST -ne 1 ]] && cp $PPT $CARDPPT
 fi
 
 
-if [[ ! -z $CORE_CLOCK && ${CORE_CLOCK[$i]} > 0 ]]; then
+if [[ ! -z $CORE_CLOCK && ${CORE_CLOCK[$i]} -gt 0 ]]; then
 	[[ -z $coreState ]] && # core set is not specified, let's use some default or it will not work
 		echo -e "${YELLOW}WARNING: Unset core state, falling back to $DEFAULT_CORE_STATE${NOCOLOR}" &&
 		coreState=$DEFAULT_CORE_STATE
-	#args+=" --core-clock ${CORE_CLOCK[$i]} --core-state $coreState"
 	for coreStateCtr in {1..7}; do #setting frequencies for all states
-		wolfamdctrl -i $cardno --core-clock ${CORE_CLOCK[$i]} --core-state $coreStateCtr
+		ohgodatool -p $PPT --core-clock ${CORE_CLOCK[$i]} --core-state $coreStateCtr
 	done
+	[[ ${CORE_CLOCK[$i]} -gt $maxCoreClock ]] &&
+		ohgodatool -p $PPT --set-max-core-clock ${CORE_CLOCK[$i]}
+	[[ $FAST -ne 1 ]] && cp $PPT $CARDPPT
 fi
 
 
-if [[ ! -z $CORE_VDDC && ${CORE_VDDC[$i]} > 0 ]]; then
+if [[ ! -z $CORE_VDDC && ${CORE_VDDC[$i]} -gt 0 ]]; then
 	[[ -z $coreState ]] && # core set is not specified, let's use some default or it will not work
 		echo -e "${YELLOW}WARNING: Unset core state, falling back to $DEFAULT_CORE_STATE${NOCOLOR}" &&
 		coreState=$DEFAULT_CORE_STATE
-	#args+=" --vddc-table-set ${CORE_VDDC[$i]} --volt-state $coreState"
 	for voltStateCtr in {1..15}; do #setting undervolt for all volt states
-		wolfamdctrl -i $cardno --vddc-table-set ${CORE_VDDC[$i]} --volt-state $voltStateCtr
+		ohgodatool -p $PPT --vddc-table-set ${CORE_VDDC[$i]} --volt-state $voltStateCtr
 	done
+	[[ $FAST -ne 1 ]] && cp $PPT $CARDPPT
 fi
 
 
-#[[ ! -z $CORE_VDDC_INDEX && ${CORE_VDDC_INDEX[$i]} > 0 ]] &&
-#	wolfamdctrl -i $cardno --core-vddc-idx ${CORE_VDDC_INDEX[$i]} --core-state $coreState
-
-	echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable 
-
-[[ ! -z $FAN && ${FAN[$i]} > 0 ]] && wolfamdctrl -i $cardno --set-fanspeed ${FAN[$i]}
-#		args+=" --set-fanspeed ${FAN[$i]}"
-[[ ! -z $REF && ${REF[$i]} > 0 ]] && amdmemtweak --gpu $card_idx --REF ${REF[$i]}
+[[ $FAST -eq 1 ]] &&
+	echo "Applying all changes to Power Play table" &&
+	cp $PPT $CARDPPT
 
 
-#if [[ ! -z $args ]]; then
-#	cp $savedpp /sys/class/drm/card$cardno/device/pp_table
-#	sync
-#
-#	#wolfamdctrl -i $cardno --core-state $coreState --mem-state $memoryState --core-clock 1100 --mem-clock 2000
-#	oc_cmd="wolfamdctrl -i $cardno $args"
-#	echo $oc_cmd
-#	eval $oc_cmd
-#fi
+# this must be disabled for R9
+echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
+[[ ! -z $FAN && ${FAN[$i]} > 0 ]] && ohgodatool -i $cardno --set-fanspeed ${FAN[$i]}
+
+[[ ! -z $REF && ${REF[$i]} > 0 ]] && amdmemtweak --gpu $i --REF ${REF[$i]}
+
 
 
 ##original fix from http://forum.hiveos.farm/discussion/721/how-to-undervolt-really-low
--- hive/sbin/amd-oc.classic.sh
+++ hive/sbin/amd-oc.classic.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 
-#coreState=`wolfamdctrl -i $cardno --show-core | grep -E "DPM state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/'`
+# applying modified pp_table to driver without restoring original one
+# just for testing purposes
+SLOW=0
+
+PPT=/tmp/pp_table$cardno
+CARDPPT=/sys/class/drm/card$cardno/device/pp_table
+
+# using saved Power Play table
+cp $savedpp $PPT
+
+#coreState=`ohgodatool -p $PPT --show-core | grep -E "DPM state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/'`
 args=''
 coreState=
 
-#memoryState=
-#memoryState=1 #hello ethos
-#getting max mem state
-memoryState=`wolfamdctrl -i $cardno --show-mem | grep -E "Memory state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/'`
+# getting max mem state
+#maxMemoryState=`ohgodatool -p $PPT --show-mem | grep -E "Memory state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/'`
+maxMemoryState=`ohgodatool -p $PPT --show-mem | grep -oP "Memory state \K[0-9]+" | tail -n 1`
+maxMemoryClock=`ohgodatool -p $PPT --show-max-mem-clock | grep -oP "\K[0-9]+"`
+maxCoreClock=`ohgodatool -p $PPT --show-max-core-clock | grep -oP "\K[0-9]+"`
+maxPower=`ohgodatool -p $PPT --show-max-power | grep -oP "\K[0-9]+"`
+TDP=`ohgodatool -p $PPT --show-tdp | grep -oP "\K[0-9]+"`
+TDC=`ohgodatool -p $PPT --show-tdc | grep -oP "\K[0-9]+"`
+#echo "Max core: ${maxCoreClock}MHz, Max mem: ${maxMemoryClock}MHz"
+echo "TDP: ${TDP}W, TDC: ${TDC}A, Max power: ${maxPower}W"
+
 
 if [[ ! -z $CORE_STATE ]]; then
 	if [[ ${CORE_STATE[$i]} -ge 0 && ${CORE_STATE[$i]} -le 7 ]]; then
@@ -20,32 +37,30 @@ if [[ ! -z $CORE_STATE ]]; then
 fi
 
 
-if [[ ! -z $MEM_STATE ]]; then
-	if [[ ${MEM_STATE[$i]} -ge 0 && ${MEM_STATE[$i]} -le $memoryState ]]; then
-		[[ ${MEM_STATE[$i]} != 0 ]] && #skip zero state, means auto
-			memoryState=${MEM_STATE[$i]}
-	else
-		echo -e "${YELLOW}WARNING: Invalid mem state ${MEM_STATE[$i]}, falling back to $memoryState${NOCOLOR}"
-	fi
+if [[ -z ${MEM_STATE[$i]} || ${MEM_STATE[$i]} -le 0 || ${MEM_STATE[$i]} -gt $maxMemoryState ]]; then
+	[[ ! -z ${MEM_STATE[$i]} && ${MEM_STATE[$i]} -ne 0 ]] && echo -e "${YELLOW}WARNING: Invalid mem state ${MEM_STATE[$i]}, falling back to $maxMemoryState${NOCOLOR}"
+	${MEM_STATE[$i]}=$maxMemoryState
 fi
 
 
-if [[ ! -z $MEM_CLOCK && ${MEM_CLOCK[$i]} > 0 ]]; then
-	#echo "Setting mem to ${MEM_CLOCK[$i]}" &&
-	#wolfamdctrl -i $cardno --mem-clock ${MEM_CLOCK[$i]} --mem-state $memoryState
-	args+=" --mem-clock ${MEM_CLOCK[$i]} --mem-state $memoryState"
+if [[ ! -z $MEM_CLOCK && ${MEM_CLOCK[$i]} -gt 0 ]]; then
+	args+=" --mem-clock ${MEM_CLOCK[$i]} --mem-state ${MEM_STATE[$i]}"
+	[[ ${MEM_CLOCK[$i]} -gt $maxMemoryClock ]] &&
+		args+="  --set-max-mem-clock ${MEM_CLOCK[$i]}"
 fi
 
-if [[ ! -z $CORE_CLOCK && ${CORE_CLOCK[$i]} > 0 ]]; then
+
+if [[ ! -z $CORE_CLOCK && ${CORE_CLOCK[$i]} -gt 0 ]]; then
 	[[ -z $coreState ]] && # core set is not specified, let's use some default or it will not work
 		echo -e "${YELLOW}WARNING: Unset core state, falling back to $DEFAULT_CORE_STATE${NOCOLOR}" &&
 		coreState=$DEFAULT_CORE_STATE
-	#wolfamdctrl -i $cardno --core-clock ${CORE_CLOCK[$i]} --core-state $coreState
 	args+=" --core-clock ${CORE_CLOCK[$i]} --core-state $coreState"
+	[[ ${CORE_CLOCK[$i]} -gt $maxCoreClock ]] &&
+		args+=" --set-max-core-clock ${CORE_CLOCK[$i]}"
 fi
 
 
-if [[ ! -z $CORE_VDDC && ${CORE_VDDC[$i]} > 0 ]]; then
+if [[ ! -z $CORE_VDDC && ${CORE_VDDC[$i]} -gt 0 ]]; then
 	[[ -z $coreState ]] && # core set is not specified, let's use some default or it will not work
 		echo -e "${YELLOW}WARNING: Unset core state, falling back to $DEFAULT_CORE_STATE${NOCOLOR}" &&
 		coreState=$DEFAULT_CORE_STATE
@@ -53,30 +68,41 @@ if [[ ! -z $CORE_VDDC && ${CORE_VDDC[$i]} > 0 ]]; then
 fi
 
 
+if [[ ! -z $args ]]; then
+	if [[ $SLOW -ne 1 ]]; then
+		# apply changes to table
+		#echo "ohgodatool -p $PPT $args"
+		ohgodatool -p $PPT $args
 
-#[[ ! -z $CORE_VDDC_INDEX && ${CORE_VDDC_INDEX[$i]} > 0 ]] &&
-#	wolfamdctrl -i $cardno --core-vddc-idx ${CORE_VDDC_INDEX[$i]} --core-state $coreState
+		# fix mem clock setting with MDPM=1 on some gpu/driver/kernel combinations
+		if [[ ! -z ${MEM_CLOCK[$i]} && ${MEM_CLOCK[$i]} -gt 0 && ${MEM_STATE[$i]} -ne $maxMemoryState ]]; then
+			ohgodatool -p $PPT --mem-clock ${MEM_CLOCK[$i]} --mem-state $maxMemoryState
+		fi
 
+		echo "Applying all changes to Power Play table"
+		# apply changes to gpu
+		cp $PPT $CARDPPT
+	else
+		# restore saved Power Play table
+		cp $savedpp $CARDPPT
+		# apply changes to gpu
+		#echo "ohgodatool -i $cardno $args"
+		ohgodatool -i $cardno $args
 
-
-echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
-[[ ! -z $FAN && ${FAN[$i]} > 0 ]] && wolfamdctrl -i $cardno --set-fanspeed ${FAN[$i]}
-
-if [[ -z $args ]]; then
-	: #maybe there is just DPM level to set
-	#echo "Nothing to set"
+		# fix mem clock setting with MDPM=1 on some bios/gpu/driver/kernel combinations
+		if [[ ! -z ${MEM_CLOCK[$i]} && ${MEM_CLOCK[$i]} -gt 0 && ${MEM_STATE[$i]} -ne $maxMemoryState ]]; then
+			ohgodatool -i $cardno --mem-clock ${MEM_CLOCK[$i]} --mem-state $maxMemoryState
+		fi
+	fi
 else
-	cp $savedpp /sys/class/drm/card$cardno/device/pp_table
-	sync
-
-	#wolfamdctrl -i $cardno --core-state $coreState --mem-state $memoryState --core-clock 1100 --mem-clock 2000
-	oc_cmd="wolfamdctrl -i $cardno $args"
-	echo $oc_cmd
-	eval $oc_cmd
-
-	echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
-	
-	[[ ! -z $FAN && ${FAN[$i]} > 0 ]] && wolfamdctrl -i $cardno --set-fanspeed ${FAN[$i]}
-	[[ ! -z $REF && ${REF[$i]} > 0 ]] && amdmemtweak --gpu $card_idx --REF ${REF[$i]}
-	
+    # restore saved Power Play table
+    cp $savedpp $CARDPPT
 fi
+
+
+# this must be disabled for R9
+echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
+[[ ! -z $FAN && ${FAN[$i]} > 0 ]] && ohgodatool -i $cardno --set-fanspeed ${FAN[$i]}
+
+[[ ! -z $REF && ${REF[$i]} > 0 ]] && amdmemtweak --gpu $i --REF ${REF[$i]}
+
