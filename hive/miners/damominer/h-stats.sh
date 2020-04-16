f7ae4b521289a37d70e6ee4ba84a771110cdbce6
ATrump <trump.andrey@gmail.com>
2020-04-16 22:05:57 +0300

    MINER FIX damominer - fix acc|rej|inv shares stats

--- hive/miners/damominer/h-stats.sh
+++ hive/miners/damominer/h-stats.sh
@@ -16,14 +16,25 @@ function hex2dec_json() {
     local bus_numbers=`hex2dec_json "$bus_ids"`
     local api_fan=`echo $stats_raw | jq -c '[.devices[].hardware.sensors[1]]'`
     local api_temp=`echo $stats_raw | jq -c '[.devices[].hardware.sensors[0]]'`
-    local uptime=`echo $stats_raw | jq -r '.host.runtime'`
     local hs_units="hs"
+    local uptime=`echo $stats_raw | jq -r '.host.runtime'`
     local hs_hex=`echo $stats_raw | jq -r '.devices[].mining.hashrate' | cut -d x -f 2`
+
     local hs_dec=`hex2dec_json "$hs_hex"`
     khs=`echo $hs_dec | jq -r '.[]' | awk '{s+=$1} END {printf("%.4f",s/1000)}'` #"
     local hs2_hex=`echo $stats_raw | jq -r '.devices[].mining.hashrate_sc' | cut -d x -f 2`
     local hs2_dec=`hex2dec_json "$hs2_hex"`
     khs2=`echo $hs2_dec | jq -r '.[]' | awk '{s+=$1} END {printf("%.4f",s/1000)}'` #"
+
+    local acc=`echo $stats_raw | jq -r '.mining.shares[0]'`
+    local rej=`echo $stats_raw | jq -r '.mining.shares[1]'`
+    local inv=`echo $stats_raw | jq -r '.mining.shares[2]'`
+    local inv_gpu=`echo $stats_raw | jq -r '.devices[].mining.shares[2]'`
+    ing_gpu=`echo ${inv_gpu[@]} | tr " " "\;"`
+
+    local acc2=`echo $stats_raw | jq -r '.mining.shares_sc[0]'`
+    local rej2=`echo $stats_raw | jq -r '.mining.shares_sc[1]'`
+    local inv2=`echo $stats_raw | jq -r '.mining.shares_sc[2]'`
 
     local algo=; local algo2=;
     [[ -z $DAMOMINER_ALGO ]] && DAMOMINER_ALGO="ethash"
@@ -60,9 +71,13 @@ function hex2dec_json() {
                     --argjson bus_numbers "$bus_numbers" \
                     --argjson fan "$api_fan" --argjson temp "$api_temp" \
                     --argjson hs "$hs_dec" --argjson hs2 "$hs2_dec" \
+                    --arg acc "$acc" --arg rej "$rej" \
+                    --arg inv "$inv" --arg inv_gpu "$ing_gpu" \
+                    --arg acc2 "$acc2" --arg rej2 "$rej2" \
+                    --arg inv2 "$inv2"  \
                     '{$total_khs, $hs, $hs_units,
-                    $temp, $fan, $uptime, $algo,
-                    $total_khs2, $hs2, $hs_units2,
+                    $temp, $fan, $uptime, $algo, ar:[$acc, $rej, $inv, $inv_gpu],
+                    $total_khs2, $hs2, $hs_units2, ar2:[$acc2, $rej2, $inv2, $inv_gpu],
                     $algo2, $bus_numbers, $ver}')
     else
       #single mode
@@ -74,8 +89,10 @@ function hex2dec_json() {
                     --argjson bus_numbers "$bus_numbers" \
                     --argjson fan "$api_fan" --argjson temp "$api_temp" \
                     --argjson hs "$hs_dec" \
+                    --arg acc "$acc" --arg rej "$rej" \
+                    --arg inv "$inv" --arg inv_gpu "$ing_gpu" \
                     '{$total_khs, $hs, $hs_units,
-                    $temp, $fan, $uptime, $algo,
+                    $temp, $fan, $uptime, $algo, ar:[$acc, $rej, $inv, $inv_gpu],
                     $bus_numbers, $ver}')
     fi
   fi
