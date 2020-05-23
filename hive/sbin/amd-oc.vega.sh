b6de952bba9bbc5dccd9da0a297d1e2acfd662ec
HaloGenius <halogenius@yandex.ru>
2020-05-23 08:28:54 +0300

    LINUX FIX  AMD OC: for further work declaring limits for Navi as constants

--- hive/sbin/amd-oc.vega.sh
+++ hive/sbin/amd-oc.vega.sh
@@ -4,36 +4,41 @@
 VEGA20=$( lspci -vnns $busid | grep VGA -A 2 | grep AMD -A 2 | grep Vega -A 2 | grep "Vega 20" | wc -l )
 #NAVI=$( lspci -vnns $busid | grep Navi | wc -l )
 
+NAVI_VDDCI_MIN=750
+NAVI_VDDCI_MAX=850
+NAVI_MVDD_MIN=1200
+NAVI_MVDD_MAX=1350
+
 echo "manual" > /sys/class/drm/card$cardno/device/power_dpm_force_performance_level
 
 if [[ $NAVI_COUNT -ne 0 ]]; then
     args=""
-    if [[ ! -z $VDDCI && ${VDDCI[$i]} -ge 750 && ${VDDCI[$i]} -le 850 ]]; then
+    if [[ ! -z $VDDCI && ${VDDCI[$i]} -ge $NAVI_VDDCI_MIN && ${VDDCI[$i]} -le $NAVI_VDDCI_MAX ]]; then
        vlt_vddci=$((${VDDCI[$i]} * 4 ))
        args+="smcPPTable/MemVddciVoltage/1=${vlt_vddci} smcPPTable/MemVddciVoltage/2=${vlt_vddci} smcPPTable/MemVddciVoltage/3=${vlt_vddci} "
     fi
-    if [[ ! -z $MVDD && ${MVDD[$i]} -ge 1250 && ${MVDD[$i]} -le 1350 ]]; then
+    if [[ ! -z $MVDD && ${MVDD[$i]} -ge $NAVI_MVDD_MIN && ${MVDD[$i]} -le $NAVI_MVDD_MAX ]]; then
        vlt_mvdd=$((${MVDD[$i]} * 4 ))
        args+="smcPPTable/MemMvddVoltage/1=${vlt_mvdd} smcPPTable/MemMvddVoltage/2=${vlt_mvdd} smcPPTable/MemMvddVoltage/3=${vlt_mvdd} "
     fi
     python /hive/opt/upp/upp.py -i /sys/class/drm/card$cardno/device/pp_table set \
-    	smcPPTable/FanStopTemp=20 smcPPTable/FanStartTemp=25 smcPPTable/FanZeroRpmEnable=0 \
+    	smcPPTable/FanStopTemp=0 smcPPTable/FanStartTemp=0 smcPPTable/FanZeroRpmEnable=0 \
     	smcPPTable/MinVoltageGfx=2800 $args \
     	OverDrive8Table/ODSettingsMax/8=960 \
-    	OverDrive8Table/ODFeatureCapabilities/9=0 \
     	OverDrive8Table/ODSettingsMin/3=700 OverDrive8Table/ODSettingsMin/5=700 OverDrive8Table/ODSettingsMin/7=700 \
     	--write
 fi
 #    	smcPPTable/FanTargetTemperature=85 
 #    	smcPPTable/MemMvddVoltage/3=5200
 #    	smcPPTable/MemVddciVoltage/3=3200
-#	smcPPTable/FanPwmMin=35
+#		smcPPTable/FanPwmMin=35
+#    	OverDrive8Table/ODFeatureCapabilities/9=0 \
 
 function _SetcoreVDDC {
 	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0  ]]; then
 		echo "Noop"
 	else
-	vegatool -i $cardno --volt-state 7 --vddc-table-set $1 --vddc-mem-table-set $1
+		vegatool -i $cardno --volt-state 7 --vddc-table-set $1 --vddc-mem-table-set $1
 	fi
 }	
 
@@ -64,6 +69,7 @@ function _SetmemClock {
 	fi
 }	
 
+
 if [[ ! -z $MEM_CLOCK && ${MEM_CLOCK[$i]} -gt 0 ]]; then
 	_SetmemClock ${MEM_CLOCK[$i]}
 fi
@@ -83,18 +89,24 @@ fi
 	echo 5 > /sys/class/drm/card$cardno/device/pp_power_profile_mode
 	#vegatool -i $cardno --set-fanspeed 50
 	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
-		rocm-smi -d  $cardno --setfan 50%
+		rocm-smi -d $cardno --setfan 50%
 	else	
 		vegatool -i $cardno  --set-fanspeed 50
 	fi
 
 
-
-
 [[ ! -z $FAN && ${FAN[$i]} -gt 0 ]] &&
 	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
-		rocm-smi -d  $cardno --setfan ${FAN[$i]}%
+		rocm-smi -d $cardno --setfan ${FAN[$i]}%
 	else	
 		vegatool -i $cardno  --set-fanspeed ${FAN[$i]}
 	fi
-	
+
+
+if [[ ! -z $PL && ${PL[$i]} -gt 0 ]]; then
+	hwmondir=`realpath /sys/class/drm/card$cardno/device/hwmon/hwmon*/`
+	if [[ -e ${hwmondir}/power1_cap_max ]] && [[ -e ${hwmondir}/power1_cap ]]; then
+#		echo Power Limit set to ${PL[$i]} W
+		rocm-smi -d $cardno --autorespond=y --setpoweroverdrive ${PL[$i]} # --loglevel error 
+	fi
+fi
