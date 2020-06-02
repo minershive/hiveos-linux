35c57a4dc2d2d8ee6e001a0c1682f05f3aa5503c
HaloGenius <halogenius@yandex.ru>
2020-06-03 00:13:48 +0300

    MINERS DELETE Mass cleanup from old miners versions

--- hive/miners/dstm/0.6.0/zm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-[[ ! -e ./zm.conf ]] && echo "No zm.conf, exiting" && exit 1
-
-zm --cfg-file=zm.conf
--- hive/miners/xmrig/xmrig/2.13.1/xmrig.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd `dirname $0`
-
-export LD_LIBRARY_PATH=/hive/lib
-
-xmrig $@
