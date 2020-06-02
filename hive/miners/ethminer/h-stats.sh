670105968ff0ad077609f2408bdf6ab5dc83e617
HaloGenius <halogenius@yandex.ru>
2020-06-03 01:24:38 +0300

    MINERS FIX Ethminer - fixed algo map for fork `EtherCore`

--- hive/miners/ethminer/h-stats.sh
+++ hive/miners/ethminer/h-stats.sh
@@ -33,7 +33,6 @@ else
 
 	local algo="ethash"
 	[[ $ETHMINER_FORK == "kawpowminer" ]] && algo="kawpow"
-	[[ $ETHMINER_FORK == "ethercore" ]]   && algo="progpow"
 	[[ $ETHMINER_FORK == "progpow" ]]     && algo="progpow"
 	[[ $ETHMINER_FORK == "serominer" ]]   && algo="progpow"
 	[[ $ETHMINER_FORK == "ubqminer" ]]    && algo="ubqhash"
