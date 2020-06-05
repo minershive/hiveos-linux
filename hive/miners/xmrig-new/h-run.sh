b1e9b6ef06f73c7885a24e6ae357a35e447fdf4a
HaloGenius <halogenius@yandex.ru>
2020-06-06 02:21:53 +0300

    MINERS FIX XMRig: reduced RX optomization messages

--- hive/miners/xmrig-new/h-run.sh
+++ hive/miners/xmrig-new/h-run.sh
@@ -2,11 +2,12 @@
 
 # HugePages tunning
 function HugePagesTune(){
-   echo "Start HugePages tuning for RandomX ..."
    if [[ ! -z $XMRIG_NEW_HUGEPAGES ]]; then
+       echo "Start HugePages tuning for RandomX ..."
        hugepages $XMRIG_NEW_HUGEPAGES
    else
       if [[ `echo $XMRIG_NEW_ALGO | grep -c "^rx\/"` -gt 0 ]]; then
+          echo "Start HugePages tuning for RandomX ..."
           hugepages -rx
       fi
    fi
