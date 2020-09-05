05c461ab6858155bbfa8fb158c0a090d75465d5e
ATrump <trump.andrey@gmail.com>
2020-09-05 14:23:18 +0300

    MINERS FIX nanominer * fix stats

--- hive/miners/nanominer/h-stats.sh
+++ hive/miners/nanominer/h-stats.sh
@@ -95,14 +95,15 @@ else
        gpu_count=`echo $stats_raw | jq -r '."Algorithms"[].'$algo' | length'`
        let "gpu_count = gpu_count - 3"
        #loop by GPUs
-       for (( j = 0; j < $gpu_count; j++ )); do
+       for (( j = 1; j <= $gpu_count; j++ )); do
          #gpu element name = echo $stats_raw | jq -rc '."Algorithms"[].'$algo' | keys' | jq .[1]
-         t_hs=`echo $stats_raw | jq -rc '."Algorithms"[].'$algo'."GPU '$j'"."Hashrate"'`
+         t_hs=`echo $stats_raw | jq -rc '."Algorithms"[].'$algo' | to_entries['$j'].value."Hashrate"'`
          t_hs=`echo $t_hs | awk '{ printf "%.4f", $1 }'`
          hs+="$t_hs "
+         let "n = j - 1"
          #fan+=`echo $stats_raw | jq -rc '."Devices"[]."GPU '$j'"."Fan"'`" "
          #temp+=`echo $stats_raw | jq -rc '."Devices"[]."GPU '$j'"."Temperature"'`" "
-         bus_id=`echo $stats_raw | jq -rc '."Devices"[]."GPU '$j'"."Pci"' | awk '{printf("%d\n", "0x"$1)}'`
+         bus_id=`echo $stats_raw | jq -rc '."Devices"[] | to_entries['$n'].value."Pci"' | awk '{printf("%d\n", "0x"$1)}'`
          bus_ids+="$bus_id "
          local all_bus_ids_array=(`echo "$gpu_detect_json" | jq -r '[ . | to_entries[] | select(.value) | .value.busid [0:2] ] | .[]'`)
          for ((k = 0; k < ${#all_bus_ids_array[@]}; k++)); do
