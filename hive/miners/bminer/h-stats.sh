99c9e8fc05a1571f706ce03065d2aa9aeb380390
ATrump <trump.andrey@gmail.com>
2020-09-08 23:38:06 +0300

    MINERS ADD bminer * add bus_numbers

--- hive/miners/bminer/h-stats.sh
+++ hive/miners/bminer/h-stats.sh
@@ -16,8 +16,8 @@
 		local hs_units="hs"
 		[[ -z $BMINER_ALGO ]] && BMINER_ALGO="stratum"
 
-		#not working at the moment, just gives numbers from 0 to n, not bus numbers
-		#local bus_numbers=$(echo $stats_raw | jq -r '[ .miners | to_entries[] | select(.value) | .key|tonumber ]') #'
+		local dev_numbers=$(echo $stats_raw | jq -r '[ .miners | to_entries[] | select(.value) | .key|tonumber ]') #'
+		local bus_numbers=$(echo $gpu_detect_json | jq -r ".$dev_numbers.busid" |  awk '{printf("%d\n", "0x"$1)}' | jq -cs '.') #'
 
 		devices_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:${MINER_API_PORT}/api/v1/status/solver`
 		#fucking bminer sorts it's keys as numerics, not natual, e.g. "1", "10", "11", "2", fix that with sed hack by replacing "1": with "01" once again:
@@ -32,9 +32,8 @@
 			stratum_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:${MINER_API_PORT}/api/v1/status/stratum`
 			#fucking bminer sorts it's keys as numerics, not natual, e.g. "1", "10", "11", "2", fix that with sed hack by replacing "1": with "01" once again
 			stratum_raw=$(echo "$stratum_raw" | sed -E 's/"([0-9])":\s*\{/"0\1":\{/g' | jq -c --sort-keys .) #"
-			[[ $BMINER_ALGO2 == "decred" ]] && local algo2="blake14r" || local algo2="blake2s"
-			local ac2=$(jq '.stratums.'${algo2}'.accepted_shares' <<< "$stratum_raw")
-			local rj2=$(jq '.stratums.'${algo2}'.rejected_shares' <<< "$stratum_raw")
+			local ac2=$(jq '.stratums.'${BMINER_ALGO2}'.accepted_shares' <<< "$stratum_raw")
+			local rj2=$(jq '.stratums.'${BMINER_ALGO2}'.rejected_shares' <<< "$stratum_raw")
 
 			stats=$(jq -c --arg uptime "$uptime" \
 						--arg algo "$BMINER_ALGO" \
@@ -43,21 +42,25 @@
 						--arg hs_units2 "$hs_units2" \
 						--arg ac2 "$ac2" --arg rj2 "$rj2" \
 						--argjson hs2 "`echo ${hs2[@]} | tr " " "\n" | jq -cs '.'`" \
-            --arg total_khs "$khs" \
-            --arg total_khs2 "$khs2" \
-            '{$total_khs, hs: [.miners[].solver.solution_rate], $hs_units,
+						--arg total_khs "$khs" \
+						--arg total_khs2 "$khs2" \
+						--argjson bus_numbers "$bus_numbers" --argjson bus_numbers2 "$bus_numbers" \
+						'{$total_khs, hs: [.miners[].solver.solution_rate], $hs_units,
 							temp: [.miners[].device.temperature], fan: [.miners[].device.fan_speed], $uptime, $algo,
-							ar: [.stratum.accepted_shares, .stratum.rejected_shares], $total_khs2,
-							$hs2, $hs_units2, $algo2, ar2: [$ac2, $rj2], ver: .version}' <<< "$stats_raw")
+							ar: [.stratum.accepted_shares, .stratum.rejected_shares], $bus_numbers,
+							$total_khs2, $hs2, $hs_units2, $algo2, ar2: [$ac2, $rj2], $bus_numbers2,
+							ver: .version}' <<< "$stats_raw")
 		else
 			#single mode
 			stats=$(jq -c --arg uptime "$uptime" \
 						--arg algo "$BMINER_ALGO" \
 						--arg hs_units "$hs_units" \
-            --arg total_khs "$khs" \
+						--arg total_khs "$khs" \
+						--argjson bus_numbers "$bus_numbers" \
 						'{$total_khs, hs: [.miners[].solver.solution_rate], $hs_units,
 							temp: [.miners[].device.temperature], fan: [.miners[].device.fan_speed], $uptime, $algo,
-							ar: [.stratum.accepted_shares, .stratum.rejected_shares], ver: .version}' <<< "$stats_raw")
+							ar: [.stratum.accepted_shares, .stratum.rejected_shares], $bus_numbers,
+							ver: .version}' <<< "$stats_raw")
 		fi
 
 	fi
