af16d9b45d2e3aefbee40de7441409e009090069
biospb <42325079+biospb@users.noreply.github.com>
2020-12-31 00:34:59 +0300

    LINUX UPDATE amd-oc * added SoC clock and voltage adjustment for Vega and Navi;
    code refactoring;
    OC log can be print with 'amd-oc log'

---
+++ hive/sbin/amd-oc.navi.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+
+VEGA20=$( lspci -vnns $busid | grep VGA -A 2 | grep AMD -A 2 | grep Vega -A 2 | grep "Vega 20" | wc -l )
+#NAVI=$( lspci -vnns $busid | grep Navi | wc -l )
+
+NAVI_VDDCI_MIN=650
+NAVI_VDDCI_MAX=850
+NAVI_MVDD_MIN=1200
+NAVI_MVDD_MAX=1350
+NAVI_MinVoltageGfx=2800
+NAVI_MaxMemClock=960
+NAVI_MinGfxVolt=700
+
+echo "ggg" $NAVI_COUNT
+echo "manual" > /sys/class/drm/card$cardno/device/power_dpm_force_performance_level
+
+if [[ $NAVI_COUNT -ne 0 ]]; then
+    args=""
+    if [[ ! -z $VDDCI && ${VDDCI[$i]} -ge $NAVI_VDDCI_MIN && ${VDDCI[$i]} -le $NAVI_VDDCI_MAX ]]; then
+       vlt_vddci=$((${VDDCI[$i]} * 4 ))
+       args+="smc_pptable/MemVddciVoltage/1=${vlt_vddci} smc_pptable/MemVddciVoltage/2=${vlt_vddci} smc_pptable/MemVddciVoltage/3=${vlt_vddci} "
+    fi
+    if [[ ! -z $MVDD && ${MVDD[$i]} -ge $NAVI_MVDD_MIN && ${MVDD[$i]} -le $NAVI_MVDD_MAX ]]; then
+       vlt_mvdd=$((${MVDD[$i]} * 4 ))
+       args+="smc_pptable/MemMvddVoltage/1=${vlt_mvdd} smc_pptable/MemMvddVoltage/2=${vlt_mvdd} smc_pptable/MemMvddVoltage/3=${vlt_mvdd} "
+    fi
+    if [[ ! -z $SOCCLK && ${SOCCLK[$i]} ]]; then
+		args+="smc_pptable/FreqTableSocclk/1=${SOCCLK[$i]} "
+	fi
+    if [[ ! -z $SOCVDDMAX && ${SOCVDDMAX[$i]} ]]; then
+    	vlt_soc=$((${SOCVDDMAX[$i]} * 4 ))
+		args+="smc_pptable/MaxVoltageSoc=${vlt_soc} "
+	fi
+
+    # overdrive_table/max/ 0=gfxclock, 7=gfxvolt, 9=powerlimit, 8=memclock
+    python3 /hive/opt/upp2/upp.py  -p /sys/class/drm/card$cardno/device/pp_table set \
+    	smc_pptable/FanStopTemp=0 smc_pptable/FanStartTemp=0 smc_pptable/FanZeroRpmEnable=0 \
+    	smc_pptable/${NAVI_MinVoltageGfx}  \
+    	$args \
+    	overdrive_table/max/8=${NAVI_MaxMemClock} \
+    	overdrive_table/min/3=700 overdrive_table/min/5=700 \
+    	overdrive_table/min/7=${NAVI_MinGfxVolt} \
+    	--write
+fi
+#	smcPPTable/FanTargetTemperature=85 
+#	smcPPTable/MemMvddVoltage/3=5200
+#	smcPPTable/MemVddciVoltage/3=3200
+#	smcPPTable/FanPwmMin=35
+#	OverDrive8Table/ODFeatureCapabilities/9=0
+
+function _SetcoreVDDC {
+		echo "Noop"
+}	
+
+function _SetcoreClock {
+	local vdd=$2
+		echo "s 1 $1" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
+		[[  -z $vdd  ]] && vdd="950"
+		[[  -z $vdd  && $NAVI_COUNT -ne 0 ]] && vdd="0"
+		[[ $vdd -gt 725 ]] && echo "vc 1 $(($1-100)) $(($vdd-25))" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
+		echo "vc 2 $1 $vdd" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
+		echo c > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
+}	
+
+function _SetmemClock {
+		echo "m 1 $1" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
+		echo c > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
+}	
+
+
+if [[ ! -z $MEM_CLOCK && ${MEM_CLOCK[$i]} -gt 0 ]]; then
+	_SetmemClock ${MEM_CLOCK[$i]}
+fi
+
+if [[ ! -z $CORE_CLOCK && ${CORE_CLOCK[$i]} -gt 0 ]]; then
+	_SetcoreClock ${CORE_CLOCK[$i]} ${CORE_VDDC[$i]}
+fi
+
+if [[ ! -z $CORE_VDDC && ${CORE_VDDC[$i]} -gt 0 ]]; then
+	_SetcoreVDDC ${CORE_VDDC[$i]}
+fi
+
+
+	echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
+	echo "manual" > /sys/class/drm/card$cardno/device/power_dpm_force_performance_level
+	echo 5 > /sys/class/drm/card$cardno/device/pp_power_profile_mode
+
+	# set fan speed
+	hwmondir=`realpath /sys/class/drm/card$cardno/device/hwmon/hwmon*/`
+	if [[ ! -z ${hwmondir} ]]; then
+		[[ -e ${hwmondir}/pwm1_enable ]] && echo 1 > ${hwmondir}/pwm1_enable
+		if [[ ${FAN[$i]} -gt 0 && -e ${hwmondir}/pwm1 ]]; then
+		[[ -e ${hwmondir}/pwm1_max ]] && fanmax=`head -1 ${hwmondir}/pwm1_max` || fanmax=255
+			[[ -e ${hwmondir}/pwm1_min ]] && fanmin=`head -1 ${hwmondir}/pwm1_min` || fanmin=0
+		echo $(( FAN[i]*(fanmax - fanmin)/100 + fanmin )) > ${hwmondir}/pwm1
+		fi
+	else
+		echo "Error: unable to get HWMON dir to set fan"
+	fi
+
+
+
+
+if [[ ! -z $PL && ${PL[$i]} -gt 0 ]]; then
+	hwmondir=`realpath /sys/class/drm/card$cardno/device/hwmon/hwmon*/`
+	if [[ -e ${hwmondir}/power1_cap_max ]] && [[ -e ${hwmondir}/power1_cap ]]; then
+#		echo Power Limit set to ${PL[$i]} W
+		rocm-smi -d $cardno --autorespond=y --setpoweroverdrive ${PL[$i]} # --loglevel error 
+	fi
+fi
--- hive/sbin/amd-oc.vega.sh
+++ hive/sbin/amd-oc.vega.sh
@@ -1,115 +1,162 @@
 #!/usr/bin/env bash
 
+DEFAULT_CORE_STATE=7
 
-VEGA20=$( lspci -vnns $busid | grep VGA -A 2 | grep AMD -A 2 | grep Vega -A 2 | grep "Vega 20" | wc -l )
-#NAVI=$( lspci -vnns $busid | grep Navi | wc -l )
+SLEEP=1
 
-NAVI_VDDCI_MIN=650
-NAVI_VDDCI_MAX=850
-NAVI_MVDD_MIN=1200
-NAVI_MVDD_MAX=1350
-
-echo "manual" > /sys/class/drm/card$cardno/device/power_dpm_force_performance_level
-
-if [[ $NAVI_COUNT -ne 0 ]]; then
-    args=""
-    if [[ ! -z $VDDCI && ${VDDCI[$i]} -ge $NAVI_VDDCI_MIN && ${VDDCI[$i]} -le $NAVI_VDDCI_MAX ]]; then
-       vlt_vddci=$((${VDDCI[$i]} * 4 ))
-       args+="smcPPTable/MemVddciVoltage/1=${vlt_vddci} smcPPTable/MemVddciVoltage/2=${vlt_vddci} smcPPTable/MemVddciVoltage/3=${vlt_vddci} "
-    fi
-    if [[ ! -z $MVDD && ${MVDD[$i]} -ge $NAVI_MVDD_MIN && ${MVDD[$i]} -le $NAVI_MVDD_MAX ]]; then
-       vlt_mvdd=$((${MVDD[$i]} * 4 ))
-       args+="smcPPTable/MemMvddVoltage/1=${vlt_mvdd} smcPPTable/MemMvddVoltage/2=${vlt_mvdd} smcPPTable/MemMvddVoltage/3=${vlt_mvdd} "
-    fi
-    python /hive/opt/upp/upp.py -i /sys/class/drm/card$cardno/device/pp_table set \
-    	smcPPTable/FanStopTemp=0 smcPPTable/FanStartTemp=0 smcPPTable/FanZeroRpmEnable=0 \
-    	smcPPTable/MinVoltageGfx=2800 $args \
-    	OverDrive8Table/ODSettingsMax/8=960 \
-    	OverDrive8Table/ODSettingsMin/3=700 OverDrive8Table/ODSettingsMin/5=700 OverDrive8Table/ODSettingsMin/7=700 \
-    	--write
+load=
+# get gpu load and switch to fast mode if not used by miner
+if [[ -e /sys/class/drm/card$cardno/device/gpu_busy_percent ]]; then
+	load=`head -1 /sys/class/drm/card$cardno/device/gpu_busy_percent 2>/dev/null`
+elif [[ -e /sys/kernel/debug/dri/$cardno/amdgpu_pm_info ]]; then
+	load=`grep -m1 'GPU Load' /sys/kernel/debug/dri/$cardno/amdgpu_pm_info 2>/dev/null | awk '{printf("%d", $3)}'`
 fi
-#	smcPPTable/FanTargetTemperature=85 
-#	smcPPTable/MemMvddVoltage/3=5200
-#	smcPPTable/MemVddciVoltage/3=3200
-#	smcPPTable/FanPwmMin=35
-#	OverDrive8Table/ODFeatureCapabilities/9=0
+[[ $load == 0 ]] && SLEEP="0.1"
 
-function _SetcoreVDDC {
-	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0  ]]; then
-		echo "Noop"
+
+function vega10_oc() {
+
+	local MIN_CLOCK=700
+	local MIN_VOLT=750
+
+	local PPT=/tmp/pp_table$cardno
+	local CARDPPT=/sys/class/drm/card$cardno/device/pp_table
+
+	# using saved Power Play table
+	cp $savedpp $PPT
+
+	local args=""
+	local coreState=
+	local memoryState=
+	local idx
+
+	readarray -t data < <( python3 /hive/opt/upp2/upp.py -p $PPT get \
+		MclkDependencyTable/NumEntries \
+		GfxclkDependencyTable/NumEntries \
+		SocclkDependencyTable/NumEntries \
+		MaxODEngineClock \
+		MaxODMemoryClock \
+	)
+
+	maxMemoryState=$(( data[0] - 1 ))
+	maxCoreState=$(( data[1] - 1 ))
+	maxSocState=$(( data[2] - 1 ))
+	maxCoreClock=$(( data[3]/100 ))
+	maxMemoryClock=$(( data[4]/100 ))
+
+	echo "Max core: ${maxCoreClock}MHz, Max mem: ${maxMemoryClock}MHz, Max mem state: $maxMemoryState, Max core state: $maxCoreState, Max Soc state: $maxSocState"
+
+
+	# set target temp for HW autofan
+	[[ $AMD_TARGET_TEMP -gt 0 ]] &&
+		args+=" FanTable/TargetTemperature=$AMD_TARGET_TEMP"
+
+	# set bios max mem clock if needed
+	[[ ${MEM_CLOCK[$i]} -gt $maxMemoryClock && $maxMemoryClock -gt $MIN_CLOCK ]] &&
+		args+=" MaxODMemoryClock=$(( MEM_CLOCK[i]*100 ))"
+
+	# set memory voltage and clock
+	if [[ ! -z ${MVDD[$i]} &&  ${MVDD[$i]} -gt $MIN_VOLT ]];then
+		args+=" VddmemLookupTable/0/Vdd=${MVDD[$i]}"
+		#in aggr set HBM voltage with atitool too
+		mvdd=$(echo "scale=2; ${MVDD[$i]}/1000" | bc )
+		[[ $AGGRESSIVE == 1 ]] && atitool -v=silent -i=$card_idx -vddcr_hbm=$mvdd >/dev/null && 
+		echo "Setting HBM voltage  to ${mvdd}V"
+	fi
+	if [[ ! -z ${MEM_CLOCK[$i]} ]];then
+	clk=${MEM_CLOCK[$i]}
+	for idx in `eval echo  {$maxMemoryState..1}`; do
+#		[[ ${VDDCI[$i]} -gt $MIN_VOLT ]] && args+=" MclkDependencyTable/${idx}/Vddci=0"
+		[[ ${MVDD[$i]} -gt $MIN_VOLT ]] && args+=" MclkDependencyTable/${idx}/VddInd=1"
+
+		if [[ ${MEM_CLOCK[$i]} -gt $MIN_CLOCK ]]; then
+				args+=" MclkDependencyTable/${idx}/MemClk=$(( clk*100 ))"
+		fi
+		clk=$(($clk-100))
+	done
+	fi
+	# set bios max core clock if needed
+	[[ ${CORE_CLOCK[$i]} -gt $maxCoreClock && $maxCoreClock -gt $MIN_CLOCK ]] &&
+		args+=" MaxODEngineClock=$(( CORE_CLOCK[i]*100 )) "
+	
+	# set core voltage and clock
+	if [[ ! -z $CORE_VDDC && ${CORE_VDDC[$i]}  -gt $MIN_VOLT  ]]; then
+		for coreState in {0..7}; do
+			args+=" VddcLookupTable/$coreState/Vdd=${CORE_VDDC[$i]} "
+		done
+	fi
+	if [[ ! -z $CORE_CLOCK && ${CORE_CLOCK[$i]} -gt $MIN_CLOCK ]]; then
+		clk=${CORE_CLOCK[$i]}
+		for idx in `eval echo  {$maxCoreState..0}`; do
+			args+="GfxclkDependencyTable/$idx/Clk=${clk}00 "
+			clk=$(($clk-20))
+		done
+		#in aggressive mode use soc OC
+		if [[  ! -z $SOCCLK && ${SOCCLK[$i]} -gt 800 ]];then
+		clk=$((${SOCCLK[$i]}+20))
+			for idx in `eval echo  {$maxSocState..0}`; do
+				args+="SocclkDependencyTable/$idx/Clk=${clk}00 "
+				clk=$(($clk-10))
+		 	done
+		fi
+	fi
+
+
+
+
+	# apply all changes to PPT and check if they differ from already applied
+	local output=""
+	[[ ! -z "$args" ]] && output=`python3 /hive/opt/upp2/upp.py -p $PPT set $args --write >/dev/null`
+	# do not apply the same table again
+#	if cmp $PPT $CARDPPT ; then
+#		echo "${GREEN}All changes were already applied to Power Play table $NOCOLOR"
+	# compare to original one
+#	elif cmp $PPT $savedpp >/dev/null; then
+#		echo "${CYAN}Restoring original Power Play table $NOCOLOR"
+#		cp $PPT $CARDPPT && sleep $SLEEP
+	# apply PPT to GPU
+#	else
+#		echo "$output"
+		echo "${CYAN}Applying all changes to Power Play table $NOCOLOR"
+		cp $PPT $CARDPPT && sleep $SLEEP
+		cat /sys/class/drm/card$cardno/device/pp_od_clk_voltage
+	#in aggr set Core/Soc voltage with atitool too
+	[[ $AGGRESSIVE == 1 &&  ${CORE_VDDC[$i]} -gt $MIN_VOLT  ]] && vdd=$(echo "scale=2; ${CORE_VDDC[$i]}/1000" | bc -l) &&
+			 		atitool -v=silent -i=$card_idx -vddcr_soc=$vdd >/dev/null && 
+			 		echo "Setting Core/Soc voltage  to ${vdd}V"
+#	fi
+
+	#memtweak if exist
+	[[ ! -z $TWEAKS && `echo $TWEAKS | jq --arg gpu "$card_idx" -r -c '. | .amdmemtweak[$gpu|tonumber] | .cmdline'` != "null" ]] &&
+		amdmemtweak --gpu $card_idx `echo $TWEAKS | jq --arg gpu "$card_idx" -r -c '. | .amdmemtweak[$gpu|tonumber] | .cmdline'` || echo "No tweaks found for GPU$card_idx"
+
+		echo "manual" > /sys/class/drm/card$cardno/device/power_dpm_force_performance_level
+
+			echo "Setting DPM core state to $maxCoreState" &&
+			echo $maxCoreState > /sys/class/drm/card$cardno/device/pp_dpm_sclk
+
+					echo "Setting DPM memory state to $maxMemoryState" &&
+					echo $maxMemoryState > /sys/class/drm/card$cardno/device/pp_dpm_mclk
+	
+
+
+	# set fan speed
+	local hwmondir=`realpath /sys/class/drm/card$cardno/device/hwmon/hwmon*/`
+	if [[ ! -z ${hwmondir} ]]; then
+		[[ -e ${hwmondir}/pwm1_enable ]] && echo 1 > ${hwmondir}/pwm1_enable
+		if [[ ${FAN[$i]} -gt 0 && -e ${hwmondir}/pwm1 ]]; then
+			[[ -e ${hwmondir}/pwm1_max ]] && fanmax=`head -1 ${hwmondir}/pwm1_max` || fanmax=255
+			[[ -e ${hwmondir}/pwm1_min ]] && fanmin=`head -1 ${hwmondir}/pwm1_min` || fanmin=0
+			echo $(( FAN[i]*(fanmax - fanmin)/100 + fanmin )) > ${hwmondir}/pwm1
+		fi
 	else
-		vegatool -i $cardno --volt-state 7 --vddc-table-set $1 --vddc-mem-table-set $1
-	fi
-}	
-
-function _SetcoreClock {
-	local vdd=$2
-	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
-		echo "s 1 $1" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
-		[[  -z $vdd  ]] && vdd="1050"
-		[[  -z $vdd  && $NAVI_COUNT -ne 0 ]] && vdd="0"
-		[[ $vdd -gt 725 ]] && echo "vc 1 $(($1-100)) $(($vdd-25))" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
-		echo "vc 2 $1 $vdd" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
-		echo c > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
-
-	else
-		vegatool -i $cardno  --core-state 1 --core-clock $(($1-60))
-		vegatool -i $cardno  --core-state 2 --core-clock $(($1-50))
-		vegatool -i $cardno  --core-state 3 --core-clock $(($1-40))
-		vegatool -i $cardno  --core-state 4 --core-clock $(($1-30))
-		vegatool -i $cardno  --core-state 5 --core-clock $(($1-20))
-		vegatool -i $cardno  --core-state 6 --core-clock $(($1-10))
-		vegatool -i $cardno  --core-state 7 --core-clock $1
-	fi
-}	
-
-function _SetmemClock {
-	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
-		echo "m 1 $1" > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
-		echo c > /sys/class/drm/card$cardno/device/pp_od_clk_voltage
-	else
-		vegatool -i $cardno --mem-state 3 --mem-clock $1
-	fi
-}	
-
-
-if [[ ! -z $MEM_CLOCK && ${MEM_CLOCK[$i]} -gt 0 ]]; then
-	_SetmemClock ${MEM_CLOCK[$i]}
-fi
-
-if [[ ! -z $CORE_CLOCK && ${CORE_CLOCK[$i]} -gt 0 ]]; then
-	_SetcoreClock ${CORE_CLOCK[$i]} ${CORE_VDDC[$i]}
-fi
-
-if [[ ! -z $CORE_VDDC && ${CORE_VDDC[$i]} -gt 0 ]]; then
-	_SetcoreVDDC ${CORE_VDDC[$i]}
-fi
-
-[[ ! -z $REF && ${REF[$i]} -gt 0 ]] && amdmemtweak --gpu $card_idx --REF ${REF[$i]}
-
-	echo 1 > /sys/class/drm/card$cardno/device/hwmon/hwmon*/pwm1_enable
-	echo "manual" > /sys/class/drm/card$cardno/device/power_dpm_force_performance_level
-	echo 5 > /sys/class/drm/card$cardno/device/pp_power_profile_mode
-	#vegatool -i $cardno --set-fanspeed 50
-	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
-		rocm-smi -d $cardno --setfan 50%
-	else	
-		vegatool -i $cardno  --set-fanspeed 50
+		echo "Error: unable to get HWMON dir to set fan"
 	fi
 
 
-[[ ! -z $FAN && ${FAN[$i]} -gt 0 ]] &&
-	if [[ $VEGA20 -ne 0 || $NAVI_COUNT -ne 0 ]]; then
-		rocm-smi -d $cardno --setfan ${FAN[$i]}%
-	else	
-		vegatool -i $cardno  --set-fanspeed ${FAN[$i]}
-	fi
+	# finally set REF
+#	[[ ${REF[$i]} -gt 0 ]] && amdmemtweak --gpu "$card_idx" --REF "${REF[$i]}"
+}
 
 
-if [[ ! -z $PL && ${PL[$i]} -gt 0 ]]; then
-	hwmondir=`realpath /sys/class/drm/card$cardno/device/hwmon/hwmon*/`
-	if [[ -e ${hwmondir}/power1_cap_max ]] && [[ -e ${hwmondir}/power1_cap ]]; then
-#		echo Power Limit set to ${PL[$i]} W
-		rocm-smi -d $cardno --autorespond=y --setpoweroverdrive ${PL[$i]} # --loglevel error 
-	fi
-fi
+vega10_oc
